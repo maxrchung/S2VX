27136be64d82bcd71aabfa41f131ba3dba7afdc1
maxrchung <maxrchung@gmail.com>
2018-01-06 01:08:15 -0800

    Removed old lines; renamed new shaders to FeatherLines

--- S2VX/Grid.hpp
+++ S2VX/Grid.hpp
@@ -11,7 +11,7 @@ namespace S2VX {
 		void update(const Time& time);
 	private:
 		// https://blog.mapbox.com/drawing-antialiased-lines-with-opengl-8766f34192dc
-		std::unique_ptr<Shader> linesShader = std::make_unique<Shader>("AntiAliasLines.VertexShader", "AntiAliasLines.FragmentShader");
+		std::unique_ptr<Shader> linesShader = std::make_unique<Shader>("FeatherLines.VertexShader", "FeatherLines.FragmentShader");
 		unsigned int linesVertexArray;
 		unsigned int linesVertexBuffer;
 		float lineWidth = 1.0f;
