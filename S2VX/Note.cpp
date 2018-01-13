f9fd928645418459d6aecb580fb447299e61e3d2
maxrchung <maxrchung@gmail.com>
2018-01-13 00:41:11 -0800

    Fixed line/square shaders, looks acceptable

--- S2VX/Note.cpp
+++ S2VX/Note.cpp
@@ -3,41 +3,85 @@
 #include <glm/gtc/matrix_transform.hpp>
 namespace S2VX {
 	Note::Note(const NoteConfiguration& pConfiguration) 
-		: configuration{ pConfiguration } {
-		glGenVertexArrays(1, &linesVertexArray);
-		glGenBuffers(1, &linesVertexBuffer);
-		glBindVertexArray(linesVertexArray);
-		glBindBuffer(GL_ARRAY_BUFFER, linesVertexBuffer);
-		// Use & to get pointer to first element
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * linePointsLength, linePoints, GL_STATIC_DRAW);
+		: configuration{ pConfiguration }, adjustedPoints{ squarePoints } {
+		glGenVertexArrays(1, &squareVertexArray);
+		glGenBuffers(1, &squareVertexBuffer);
+	}
+	Note::~Note() {
+		glDeleteVertexArrays(1, &squareVertexArray);
+		glDeleteBuffers(1, &squareVertexBuffer);
+	}
+	void Note::draw(const Camera& camera) {
+		glBindVertexArray(squareVertexArray);
+		glBindBuffer(GL_ARRAY_BUFFER, squareVertexBuffer);
+		auto lineWidth = configuration.getWidth();
+		// Points need to be adjusted so that lines do not overlap
+		// Right
+		for (int i = 1; i < 24; i += 4) {
+			if (adjustedPoints[i] > 0) {
+				adjustedPoints[i] = squarePoints[i] + lineWidth - 0.01f;
+			}
+			else {
+				adjustedPoints[i] = squarePoints[i] - lineWidth + 0.01f;
+			}
+		}
+		// Bottom
+		for (int i = 24; i < 48; i += 4) {
+			if (adjustedPoints[i] > 0) {
+				adjustedPoints[i] = squarePoints[i] + lineWidth - 0.01f;
+			}
+			else {
+				adjustedPoints[i] = squarePoints[i] - lineWidth + 0.01f;
+			}
+		}
+		// Left
+		for (int i = 49; i < 72; i += 4) {
+			if (adjustedPoints[i] > 0) {
+				adjustedPoints[i] = squarePoints[i] + lineWidth - 0.01f;
+			}
+			else {
+				adjustedPoints[i] = squarePoints[i] - lineWidth + 0.01f;
+			}
+		}
+		// Top
+		for (int i = 72; i < 96; i += 4) {
+			if (adjustedPoints[i] > 0) {
+				adjustedPoints[i] = squarePoints[i] + lineWidth - 0.01f;
+			}
+			else {
+				adjustedPoints[i] = squarePoints[i] - lineWidth + 0.01f;
+			}
+		}
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * squarePointsLength, adjustedPoints.data(), GL_STATIC_DRAW);
 		// Position
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
 		// Normal
 		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 		glEnableVertexAttribArray(1);
-	}
-	Note::~Note() {
-		glDeleteVertexArrays(1, &linesVertexArray);
-		glDeleteBuffers(1, &linesVertexBuffer);
-	}
-	void Note::draw(const Camera& camera) {
-		glBindVertexArray(linesVertexArray);
-		linesShader->use();
-		glm::mat4 model;
-		model = glm::translate(model, glm::vec3(configuration.getPosition(), 0.0f));
-		model = glm::scale(model, glm::vec3(activeScale, activeScale, 1.0f));
-		linesShader->setFloat("lineWidth", configuration.getWidth());
-		linesShader->setMat4("model", model);
-		linesShader->setMat4("view", camera.getView());
-		linesShader->setMat4("projection", camera.getProjection());
-		linesShader->setFloat("fade", 1.0f);
-		linesShader->setFloat("feather", configuration.getFeather());
-		linesShader->use();
-		glDrawArrays(GL_TRIANGLES, 0, linePointsLength / 4);
+		// Static square
+		squareShader->use();
+		glm::mat4 innerModel;
+		innerModel = glm::translate(innerModel, glm::vec3(configuration.getPosition(), 0.0f));
+		squareShader->setFloat("lineWidth", lineWidth);
+		squareShader->setMat4("model", innerModel);
+		squareShader->setMat4("view", camera.getView());
+		squareShader->setMat4("projection", camera.getProjection());
+		squareShader->setFloat("fade", 1.0f);
+		squareShader->setFloat("feather", 0.01f);
+		squareShader->use();
+		glDrawArrays(GL_TRIANGLES, 0, squarePointsLength / 4);
+		// Approaching square
+		glm::mat4 outerModel;
+		outerModel = glm::translate(outerModel, glm::vec3(configuration.getPosition(), 0.0f));
+		// Only scale for outer square
+		outerModel = glm::scale(outerModel, glm::vec3(activeScale, activeScale, 1.0f));
+		squareShader->setMat4("model", outerModel);
+		glDrawArrays(GL_TRIANGLES, 0, squarePointsLength / 4);
+
 	}
 	void Note::update(int time) {
 		auto interpolation = static_cast<float>(time - configuration.getStart()) / (configuration.getEnd() - configuration.getStart());
-		activeScale = glm::mix(0.5f, configuration.getDistance(), interpolation);
+		activeScale = glm::mix(configuration.getDistance(), 0.5f, interpolation);
 	}
 }
--- S2VX/Note.hpp
+++ S2VX/Note.hpp
@@ -2,6 +2,7 @@
 #include "Camera.hpp"
 #include "NoteConfiguration.hpp"
 #include "Shader.hpp"
+#include <array>
 namespace S2VX {
 	class Note {
 	public:
@@ -11,44 +12,45 @@ namespace S2VX {
 		void update(int time);
 	private:
 		NoteConfiguration configuration;
-		std::unique_ptr<Shader> linesShader = std::make_unique<Shader>("Square.VertexShader", "Line.FragmentShader");
-		unsigned int linesVertexArray;
-		unsigned int linesVertexBuffer;
-		static constexpr unsigned int linePointsLength = 96;
-		static constexpr float linePoints[linePointsLength] = {
+		std::unique_ptr<Shader> squareShader = std::make_unique<Shader>("Square.VertexShader", "Line.FragmentShader");
+		unsigned int squareVertexArray;
+		unsigned int squareVertexBuffer;
+		static constexpr unsigned int squarePointsLength = 96;
+		static constexpr std::array<float, squarePointsLength> squarePoints = {
 			// Position			// Normal
-			 0.5f,	 0.5f,		 1.0f,	 0.0f,	// Right triangle top-right
-			 0.5f,	-0.5f,		 1.0f,	 0.0f,	// Right triangle top-right
-			 0.5f,	 0.5f,		-1.0f,	 0.0f,	// Right triangle top-right
+			 0.5f,	 0.5f,		 1.0f,	 0.0f,	// Right triangle top-right		0
+			 0.5f,	-0.5f,		 1.0f,	 0.0f,	// Right triangle top-right		4
+			 0.5f,	 0.5f,		-1.0f,	 0.0f,	// Right triangle top-right		8
 
-			 0.5f,	-0.5f,		 1.0f,	 0.0f,	// Right triangle bottom-left
-			 0.5f,	-0.5f,		-1.0f,	 0.0f,	// Right triangle bottom-left
-			 0.5f,	 0.5f,		-1.0f,	 0.0f,	// Right triangle bottom-left
+			 0.5f,	-0.5f,		 1.0f,	 0.0f,	// Right triangle bottom-left	12
+			 0.5f,	-0.5f,		-1.0f,	 0.0f,	// Right triangle bottom-left	16
+			 0.5f,	 0.5f,		-1.0f,	 0.0f,	// Right triangle bottom-left	20
 
-			 0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle top-right
-			 0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle top-right
-			-0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle top-right
+			 0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle top-right	24
+			 0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle top-right	28
+			-0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle top-right	32
 
-			 0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle bottom-left
-			-0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle bottom-left
-			-0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle bottom-left
+			 0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle bottom-left	36
+			-0.5f,	-0.5f,		 0.0f,	-1.0f,	// Bottom triangle bottom-left	40
+			-0.5f,	-0.5f,		 0.0f,	 1.0f,	// Bottom triangle bottom-left	44
 
-			-0.5f,	 0.5f,		 1.0f,	 0.0f,	// Left triangle top-right
-			-0.5f,	-0.5f,		 1.0f,	 0.0f,	// Left triangle top-right
-			-0.5f,	 0.5f,		-1.0f,	 0.0f,	// Left triangle top-right
+			-0.5f,	 0.5f,		 1.0f,	 0.0f,	// Left triangle top-right		48
+			-0.5f,	-0.5f,		 1.0f,	 0.0f,	// Left triangle top-right		52
+			-0.5f,	 0.5f,		-1.0f,	 0.0f,	// Left triangle top-right		56
 
-			-0.5f,	-0.5f,		 1.0f,	 0.0f,	// Left triangle bottom-left
-			-0.5f,	-0.5f,		-1.0f,	 0.0f,	// Left triangle bottom-left
-			-0.5f,	 0.5f,		-1.0f,	 0.0f,	// Left triangle bottom-left
+			-0.5f,	-0.5f,		 1.0f,	 0.0f,	// Left triangle bottom-left	60
+			-0.5f,	-0.5f,		-1.0f,	 0.0f,	// Left triangle bottom-left	64
+			-0.5f,	 0.5f,		-1.0f,	 0.0f,	// Left triangle bottom-left	68
 
-			 0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle top-right
-			 0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle top-right
-			-0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle top-right
+			 0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle top-right		72
+			 0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle top-right		76
+			-0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle top-right		80
 
-			 0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle bottom-left
-			-0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle bottom-left
-			-0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle bottom-left;
+			 0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle bottom-left		84
+			-0.5f,	 0.5f,		 0.0f,	-1.0f,	// Top triangle bottom-left		88
+			-0.5f,	 0.5f,		 0.0f,	 1.0f,	// Top triangle bottom-left		92
 		};
+		std::array<float, squarePointsLength> adjustedPoints;
 		// Interpolated distance
 		float activeScale;
 	};
