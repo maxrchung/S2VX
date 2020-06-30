8eee073b13e6e781b83532ab7dd17a7cecba190d
maxrchung <maxrchung@gmail.com>
2020-06-29 22:34:02 -0700

    Moved Grid to its own class

---
+++ S2VX.Game/Grid.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osuTK;
+using osuTK.Graphics;
+
+namespace S2VX.Game
+{
+    public class Grid : CompositeDrawable
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            RelativeSizeAxes = Axes.Both;
+
+            var grid = new List<Drawable>();
+            for (float i = 0.05f; i <= 0.75f; i += 0.1f)
+            {
+                grid.Add(new RelativeBox
+                {
+                    Colour = Color4.White,
+                    Y = i,
+                    Width = 1.5f,
+                    Height = 0.005f
+                });
+                grid.Add(new RelativeBox
+                {
+                    Colour = Color4.White,
+                    Y = -i,
+                    Width = 1.5f,
+                    Height = 0.005f
+                });
+                grid.Add(new RelativeBox
+                {
+                    Colour = Color4.White,
+                    X = i,
+                    Width = 0.005f,
+                    Height = 1.5f
+                });
+                grid.Add(new RelativeBox
+                {
+                    Colour = Color4.White,
+                    X = -i,
+                    Width = 0.005f,
+                    Height = 1.5f
+                });
+            }
+
+            InternalChildren = grid;
+        }
+
+        protected override void Update()
+        {
+            var prevPosition = Position;
+            if (prevPosition == Position)
+            {
+                return;
+            }
+        }
+    }
+}
--- S2VX.Game/MainScreen.cs
+++ S2VX.Game/MainScreen.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Screens;
@@ -14,67 +15,15 @@ namespace S2VX.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            var elements = new List<Drawable>
+            InternalChildren = new Drawable[]
             {
                 new Box
                 {
                     Colour = Color4.CornflowerBlue,
                     RelativeSizeAxes = Axes.Both
-                }
+                },
+                new Grid()
             };
-
-            for (float i = 0.05f; i <= 0.5f; i += 0.1f)
-            {
-                elements.Add(new Box
-                {
-                    Colour = Color4.White,
-                    RelativePositionAxes = Axes.Both,
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Y = i,
-                    Width = 1.5f,
-                    Height = 0.005f
-                });
-
-                elements.Add(new Box
-                {
-                    Colour = Color4.White,
-                    RelativePositionAxes = Axes.Both,
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    Y = -i,
-                    Width = 1.5f,
-                    Height = 0.005f
-                });
-
-                elements.Add(new Box
-                {
-                    Colour = Color4.White,
-                    RelativePositionAxes = Axes.Both,
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    X = i,
-                    Width = 0.005f,
-                    Height = 1.5f
-                });
-
-                elements.Add(new Box
-                {
-                    Colour = Color4.White,
-                    RelativePositionAxes = Axes.Both,
-                    RelativeSizeAxes = Axes.Both,
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
-                    X = -i,
-                    Width = 0.005f,
-                    Height = 1.5f
-                });
-            }
-
-            InternalChildren = elements;
         }
     }
 }
