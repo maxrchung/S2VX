60327a727d4f13a76e43cfa064b6d5016c976d56
Naranja-Sagged <alexliuying@gmail.com>
2020-07-23 04:53:12 -0700

    Remove extra TimelineSlider drag mechanic

--- S2VX.Game/TimelineSlider.cs
+++ S2VX.Game/TimelineSlider.cs
@@ -10,8 +10,6 @@ namespace S2VX.Game
 {
     public class TimelineSlider : Box
     {
-        [Resolved]
-        private Story Story { get; set; }
         private float lineWidthUnit = 2f / 3f;
 
         public TimelineSlider()
@@ -19,16 +17,6 @@ namespace S2VX.Game
             RelativeSizeAxes = Axes.Both;
             Anchor = Anchor.CentreLeft;
             Origin = Anchor.Centre;
-        }
-
-        protected override bool OnDragStart(DragStartEvent e) => true;
-
-        protected override void OnDrag(DragEvent e)
-        {
-            var newX = X + ToLocalSpace(e.ScreenSpaceMousePosition).X;
-            var lineWidth = lineWidthUnit * Story.DrawWidth;
-            newX = Math.Clamp(newX, 0, lineWidth);
-            X = newX;
         }
     }
 }
