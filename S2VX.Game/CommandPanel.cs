6b24af4bc3c096eec273d523e4b94f6df699a615
maxrchung <maxrchung@gmail.com>
2020-07-30 23:08:38 -0700

    Add multiple inputs to fill container

--- S2VX.Game/CommandPanel.cs
+++ S2VX.Game/CommandPanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using osu.Framework.Allocation;
 using osu.Framework.Bindables;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
@@ -27,16 +28,15 @@ namespace S2VX.Game
         [BackgroundDependencyLoader]
         private void load()
         {
-            Anchor = Anchor.Centre;
-            Origin = Anchor.Centre;
+            Anchor = Anchor.CentreRight;
+            Origin = Anchor.CentreRight;
             RelativeSizeAxes = Axes.Both;
-            RelativePositionAxes = Axes.Both;
             Width = 0.5f;
-            Height = 0.5f;
+            Height = 1;
 
-            var dropdown = new BasicDropdown<string>()
+            var dropdown = new BasicDropdown<string>
             {
-                Width = 200
+                Width = 150
             };
             var allCommands = new List<string> {
                 "All Commands"
@@ -44,36 +44,71 @@ namespace S2VX.Game
             allCommands.AddRange(Enum.GetNames(typeof(Commands)));
             dropdown.Items = allCommands;
 
-            var inputsRow = new FillFlowContainer
-            {
-                new Container
-                {
-                    new SpriteText()
-                    {
-                        Text = "Command"
-                    },
-                    dropdown,
-                }
-            };
-
             Children = new Drawable[]
             {
-                inputsRow
-                //new BasicButton
-                //{
-                //    BackgroundColour = Color4.White,
-                //    Text = "+",
-                //    Width = 200,
-                //    Height = 200,
-                //},
-                //new BasicPasswordTextBox(),
-                //new BasicRearrangeableListContainer<int>(),
-                //new BasicSliderBar<int>()
-                //{
-                //    Current = sliderCurrent
-                //},
-                //new BasicTextBox(),
+                new RelativeBox
+                {
+                    Colour = Color4.Black.Opacity(0.9f)
+                },
+                new FillFlowContainer
+                {
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Direction = FillDirection.Vertical,
+                    Children = new Drawable[]
+                    {
+                        new SpriteText
+                        {
+                            Text = "Command Panel"
+                        },
+                        new Container
+                        {
+                            RelativeSizeAxes = Axes.X,
+                            AutoSizeAxes = Axes.Y,
+                            Children = new Drawable[]
+                            {
+                                new FillFlowContainer
+                                {
+                                    RelativeSizeAxes = Axes.Y,
+                                    AutoSizeAxes = Axes.X,
+                                    Direction = FillDirection.Horizontal,
+                                    Children = new Drawable[]
+                                    {
+                                        new FillFlowContainer
+                                        {
+                                            RelativeSizeAxes = Axes.Y,
+                                            AutoSizeAxes = Axes.X,
+                                            Direction = FillDirection.Vertical,
+                                            Children = new Drawable[]
+                                            {
+                                                new SpriteText { Text = "Command Type" },
+                                                dropdown
+                                            }
+                                        },
+                                        new FillFlowContainer
+                                        {
+                                           RelativeSizeAxes = Axes.Y,
+                                            AutoSizeAxes = Axes.X,
+                                            Direction = FillDirection.Vertical,
+                                            Children = new Drawable[]
+                                            {
+                                                new SpriteText { Text = "StartTime" },
+                                                new BasicTextBox
+                                                {
+                                                    Width = 100,
+                                                    Height = 30,
+                                                }
+                                            }
+                                        },
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             };
         }
     }
+
+
 }
