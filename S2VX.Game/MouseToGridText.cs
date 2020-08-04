b76268ba6782439b7656188646a078a394cb9646
maxrchung <maxrchung@gmail.com>
2020-08-03 21:50:23 -0700

    Add MouseToGridText class

---
+++ S2VX.Game/MouseToGridText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+
+namespace S2VX.Game
+{
+    public class MouseToGridText : SpriteText
+    {
+        [Resolved]
+        private Story story { get; set; } = new Story();
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Text = " ";
+        }
+
+        protected override void Update()
+        {
+            Text = "(300, 300)";
+        }
+    }
+}
