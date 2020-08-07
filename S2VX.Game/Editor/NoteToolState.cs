0df15f571014ae1c2328b92e917f2bcc4adf4cb2
maxrchung <maxrchung@gmail.com>
2020-08-07 01:38:42 -0700

    Started creating Editor ToolState classes

---
+++ S2VX.Game/Editor/NoteToolState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2VX.Game.Editor
+{
+    public class NoteToolState : ToolState
+    {
+    }
+}
---
+++ S2VX.Game/Editor/ToolState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2VX.Game.Editor
+{
+    // For more flexibility in handling a selected editor tool, we can use a class
+    // instead of a simpler enum. See https://gameprogrammingpatterns.com/state.html
+    // for some background and additional details.
+    public abstract class ToolState
+    {
+        // These handlers return whether to block propagation. By default, we return
+        // false to allow a parent component, e.g. S2VXEditor, to handle inputs.
+        public virtual bool OnMouseMove()
+        {
+            return false;
+        }
+
+        public virtual bool OnMouseClick()
+        {
+            return false;
+        }
+
+        public virtual void Update() {
+        }
+    }
+}
