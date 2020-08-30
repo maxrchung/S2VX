e649e3f8b47be5961474c3ac518fa9bb773dc6f3
maxrchung <maxrchung@gmail.com>
2020-08-30 10:55:09 -0700

    Update Approaches from commands

--- S2VX.Game/Story/Command.cs
+++ S2VX.Game/Story/Command.cs
@@ -401,7 +401,7 @@ namespace S2VX.Game.Story {
         public float EndValue { get; set; } = 0.5f;
         public override void Apply(double time, S2VXStory story) {
             var distance = Interpolation.ValueAt(time, StartValue, EndValue, StartTime, EndTime, Easing);
-            story.Notes.ApproachDistance = distance;
+            story.Approaches.Distance = distance;
         }
         protected override string ToValues() => $"{StartValue}|{EndValue}";
         public static ApproachesDistanceCommand FromString(string[] split) {
@@ -419,7 +419,7 @@ namespace S2VX.Game.Story {
         public float EndValue { get; set; } = 0.005f;
         public override void Apply(double time, S2VXStory story) {
             var thickness = Interpolation.ValueAt(time, StartValue, EndValue, StartTime, EndTime, Easing);
-            story.Notes.ApproachThickness = thickness;
+            story.Approaches.Thickness = thickness;
         }
         protected override string ToValues() => $"{StartValue}|{EndValue}";
         public static ApproachesThicknessCommand FromString(string[] split) {
