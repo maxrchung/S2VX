5b077e7147102ae521f99071a554a380b87d6267
maxrchung <maxrchung@gmail.com>
2020-09-01 00:09:44 -0700

    Add ReversibleAddNote and ReversibleRemoveNote

---
+++ S2VX.Game/Editor/Reversible/ReversibleAddNote.cs
@@ -0,0 +1,18 @@
+﻿using osu.Framework.Allocation;
+using S2VX.Game.Story;
+using System;
+
+namespace S2VX.Game.Editor.Reversible {
+    public class AddNoteReversible : IReversible {
+        [Resolved]
+        private S2VXStory Story { get; set; }
+
+        private Note Note { get; }
+
+        public AddNoteReversible(Note note) => Note = note;
+
+        public void Undo() => Story.RemoveNote(Note);
+
+        public void Redo() => Story.AddNote(Note.Position, Note.EndTime);
+    }
+}
---
+++ S2VX.Game/Editor/Reversible/ReversibleRemoveNote.cs
@@ -0,0 +1,17 @@
+﻿using osu.Framework.Allocation;
+using S2VX.Game.Story;
+
+namespace S2VX.Game.Editor.Reversible {
+    public class RemoveNoteReversible : IReversible {
+        [Resolved]
+        private S2VXStory Story { get; set; }
+
+        private Note Note { get; }
+
+        public RemoveNoteReversible(Note note) => Note = note;
+
+        public void Undo() => Story.AddNote(Note.Position, Note.EndTime);
+
+        public void Redo() => Story.RemoveNote(Note);
+    }
+}
