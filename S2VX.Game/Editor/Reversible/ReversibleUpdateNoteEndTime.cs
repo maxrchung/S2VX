9e3e0069bef748c1695ee27ff95a0d3456d464c2
maxrchung <maxrchung@gmail.com>
2020-09-01 20:50:02 -0700

    Reversible for adding note end time

---
+++ S2VX.Game/Editor/Reversible/ReversibleUpdateNoteEndTime.cs
@@ -0,0 +1,19 @@
+﻿using S2VX.Game.Story;
+
+namespace S2VX.Game.Editor.Reversible {
+    public class ReversibleUpdateNoteEndTime : IReversible {
+        private Note Note { get; }
+        private double OldEndTime { get; }
+        private double NewEndTime { get; }
+
+        public ReversibleUpdateNoteEndTime(Note note, double endTime) {
+            Note = note;
+            OldEndTime = note.EndTime;
+            NewEndTime = endTime;
+        }
+
+        public void Undo() => Note.UpdateEndTime(OldEndTime);
+
+        public void Redo() => Note.UpdateEndTime(NewEndTime);
+    }
+}
