36c60d62a38a0e191b092c400cb757292b80f739
maxrchung <maxrchung@gmail.com>
2020-09-01 20:52:09 -0700

    Add reversible for updating coordinates

---
+++ S2VX.Game/Editor/Reversible/ReversibleUpdateNoteCoordinates.cs
@@ -0,0 +1,20 @@
+﻿using osuTK;
+using S2VX.Game.Story;
+
+namespace S2VX.Game.Editor.Reversible {
+    public class ReversibleUpdateNoteCoordinates : IReversible {
+        private Note Note { get; }
+        private Vector2 OldCoordinates { get; }
+        private Vector2 NewCoordinates { get; }
+
+        public ReversibleUpdateNoteCoordinates(Note note, Vector2 coordinates) {
+            Note = note;
+            OldCoordinates = note.Coordinates;
+            NewCoordinates = coordinates;
+        }
+
+        public void Undo() => Note.UpdateCoordinates(OldCoordinates);
+
+        public void Redo() => Note.UpdateCoordinates(NewCoordinates);
+    }
+}
