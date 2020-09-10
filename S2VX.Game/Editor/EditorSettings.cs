fe5a5dec6c5f1b14e9c6a1c0b086e8a7218f7f15
Xenocidel <aliao4@uci.edu>
2020-09-09 19:58:03 -0700

    removed unncessary Json attributes

--- S2VX.Game/Editor/EditorSettings.cs
+++ S2VX.Game/Editor/EditorSettings.cs
@@ -1,22 +1,10 @@
-﻿using Newtonsoft.Json;
-
-namespace S2VX.Game.Editor {
-    [JsonObject]
+﻿namespace S2VX.Game.Editor {
     public class EditorSettings {
         // Default editor settings for new projects
-        [JsonProperty(PropertyName = "TrackTime")]
         public double TrackTime { get; set; }
-
-        [JsonProperty(PropertyName = "TrackVolume")]
         public double TrackVolume { get; set; } = 0.2;
-
-        [JsonProperty(PropertyName = "TrackPlaybackRate")]
         public double TrackPlaybackRate { get; set; } = 1.0;
-
-        [JsonProperty(PropertyName = "SnapDivisor")]
         public int SnapDivisor { get; set; } = 1;
-
-        [JsonProperty(PropertyName = "BeatSnapDivisorIndex")]
         public int BeatSnapDivisorIndex { get; set; } = 3;
     }
 }
