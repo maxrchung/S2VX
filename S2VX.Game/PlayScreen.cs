f323494d0564441bdcea6c318a703c6a5f0586f4
maxrchung <maxrchung@gmail.com>
2020-09-19 20:54:26 -0700

    Push and pop PlayScreen

---
+++ S2VX.Game/PlayScreen.cs
@@ -0,0 +1,41 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Audio;
+using osu.Framework.Input.Events;
+using osu.Framework.Screens;
+using osu.Framework.Timing;
+using osuTK.Input;
+using S2VX.Game.Story;
+
+namespace S2VX.Game {
+    public class PlayScreen : Screen {
+        [BackgroundDependencyLoader]
+        private void Load(AudioManager audio) {
+            var story = new S2VXStory();
+            story.Open(@"../../../story.json", false);
+            story.ClearActives();
+
+            var track = new DrawableTrack(audio.Tracks.Get(@"Camellia_MEGALOVANIA_Remix.mp3"));
+            var settings = story.GetEditorSettings();
+            track.Volume.Value = settings.TrackVolume;
+            track.Tempo.Value = settings.TrackPlaybackRate;
+            track.Seek(settings.TrackTime);
+            track.Start();
+            Clock = new FramedClock(track);
+            InternalChildren = new Drawable[] {
+                story,
+                track
+            };
+        }
+
+        protected override bool OnKeyDown(KeyDownEvent e) {
+            switch (e.Key) {
+                case Key.Escape:
+                    this.Exit();
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- S2VX.Game/S2VXGame.cs
+++ S2VX.Game/S2VXGame.cs
@@ -1,12 +1,19 @@
 ﻿using osu.Framework.Allocation;
+using osu.Framework.Screens;
 using S2VX.Game.Editor;
 
 namespace S2VX.Game {
     public class S2VXGame : S2VXGameBase {
         [Cached]
-        private readonly S2VXEditor Editor = new S2VXEditor();
+        private EditorScreen Editor { get; set; } = new EditorScreen();
+
+        [Cached]
+        private ScreenStack Screens { get; set; } = new ScreenStack();
 
         [BackgroundDependencyLoader]
-        private void Load() => Child = Editor;
+        private void Load() {
+            Screens = new ScreenStack(Editor);
+            Child = Screens;
+        }
     }
 }
