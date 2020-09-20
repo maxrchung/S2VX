66725da1730b6cf13bc9f678eb4bfd1007d05ca2
maxrchung <maxrchung@gmail.com>
2020-09-19 18:40:50 -0700

    OnClick handlers for different types of notes

--- S2VX.Game/Story/Note/EditorNote.cs
+++ S2VX.Game/Story/Note/EditorNote.cs
@@ -1,5 +1,11 @@
-﻿namespace S2VX.Game.Story.Note {
-    public class EditorNote : S2VXNote {
+﻿using osu.Framework.Input.Events;
+using System;
 
+namespace S2VX.Game.Story.Note {
+    public class EditorNote : S2VXNote {
+        protected override bool OnClick(ClickEvent e) {
+            Console.WriteLine("You clicked a EditorNote");
+            return false;
+        }
     }
 }
--- S2VX.Game/Story/Note/GameNote.cs
+++ S2VX.Game/Story/Note/GameNote.cs
@@ -1,5 +1,11 @@
-﻿namespace S2VX.Game.Story.Note {
-    public class GameNote : S2VXNote {
+﻿using osu.Framework.Input.Events;
+using System;
 
+namespace S2VX.Game.Story.Note {
+    public class GameNote : S2VXNote {
+        protected override bool OnClick(ClickEvent e) {
+            Console.WriteLine("GameNote clicked");
+            return false;
+        }
     }
 }
