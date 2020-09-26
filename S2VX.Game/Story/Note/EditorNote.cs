75c7986d2db0c2210c4ef97b9bf94ef5dea2ca3c
maxrchung <maxrchung@gmail.com>
2020-09-25 23:42:25 -0700

    Placeholders for Note hitsound

--- S2VX.Game/Story/Note/EditorNote.cs
+++ S2VX.Game/Story/Note/EditorNote.cs
@@ -1,9 +1,17 @@
-﻿using osu.Framework.Input.Events;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Input.Events;
 using System;
 
 namespace S2VX.Game.Story.Note {
     public class EditorNote : S2VXNote {
+        [Resolved]
+        private AudioManager Audio { get; set; }
+
         protected override bool OnClick(ClickEvent e) {
+            var hit = Audio.Samples.Get("hit");
+            hit.Play();
+
             Console.WriteLine("EditorNote clicked");
             return false;
         }
--- S2VX.Game/Story/Note/GameNote.cs
+++ S2VX.Game/Story/Note/GameNote.cs
@@ -1,9 +1,17 @@
-﻿using osu.Framework.Input.Events;
+﻿using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Input.Events;
 using System;
 
 namespace S2VX.Game.Story.Note {
     public class GameNote : S2VXNote {
+        [Resolved]
+        private AudioManager Audio { get; set; }
+
         protected override bool OnClick(ClickEvent e) {
+            var miss = Audio.Samples.Get("miss");
+            miss.Play();
+
             Console.WriteLine("GameNote clicked");
             return false;
         }
