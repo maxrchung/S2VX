76b4d4b656c5c02df5b2f0c4b358ee57b0bf15bf
maxrchung <maxrchung@gmail.com>
2020-10-12 12:04:07 -0700

    Add song selection screen with basic buttons to go to edit/play

--- S2VX.Game/S2VXGame.cs
+++ S2VX.Game/S2VXGame.cs
@@ -1,18 +1,15 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Screens;
-using S2VX.Game.Editor;
+using S2VX.Game.Play;
 
 namespace S2VX.Game {
     public class S2VXGame : S2VXGameBase {
-        [Cached]
-        private EditorScreen Editor { get; set; } = new EditorScreen();
-
         [Cached]
         private ScreenStack Screens { get; set; } = new ScreenStack();
 
         [BackgroundDependencyLoader]
         private void Load() {
-            Screens.Push(Editor);
+            Screens.Push(new SongSelectionScreen());
             Child = Screens;
         }
     }
---
+++ S2VX.Game/SongSelection/SongSelectionScreen.cs
@@ -0,0 +1,45 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Screens;
+using osuTK;
+using S2VX.Game.Editor;
+
+namespace S2VX.Game.Play {
+    public class SongSelectionScreen : Screen {
+        private static Vector2 InputSize { get; } = new Vector2(100, 30);
+        private static Vector2 Spacing { get; } = new Vector2(15);
+
+        [Cached]
+        private EditorScreen Editor { get; set; } = new EditorScreen();
+
+        [Resolved]
+        private ScreenStack Screens { get; set; }
+
+        private Button BtnEdit { get; } = new BasicButton() {
+            Size = InputSize,
+            Text = "Edit"
+        };
+
+        private Button BtnPlay { get; } = new BasicButton() {
+            Size = InputSize,
+            Text = "Play"
+        };
+
+        [BackgroundDependencyLoader]
+        private void Load() {
+            BtnEdit.Action = () => Screens.Push(Editor);
+            BtnPlay.Action = () => Screens.Push(new PlayScreen());
+            InternalChild = new FillFlowContainer {
+                Direction = FillDirection.Horizontal,
+                Spacing = Spacing,
+                Children = new Drawable[]
+                {
+                    BtnEdit,
+                    BtnPlay
+                }
+            };
+        }
+    }
+}
