8edbc6e8303fbae676b8cfb64ebddff9ea4cc09a
Xenocidel <aliao4@uci.edu>
2020-10-12 22:48:11 -0700

    Create stories directory and show number of folders inside it

--- S2VX.Game/SongSelection/SongSelectionScreen.cs
+++ S2VX.Game/SongSelection/SongSelectionScreen.cs
@@ -1,14 +1,23 @@
 ﻿using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Platform;
 using osu.Framework.Screens;
 using osuTK;
 using S2VX.Game.Editor;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 
 namespace S2VX.Game.Play {
     public class SongSelectionScreen : Screen {
         private static Vector2 InputSize { get; } = new Vector2(100, 30);
+
+        private static NativeStorage StoriesStorage { get; } = new NativeStorage("Stories");
+
+        private TextFlowContainer Storybooks { get; set; }
 
         private Button BtnEdit { get; } = new BasicButton() {
             Size = InputSize,
@@ -29,9 +38,18 @@ namespace S2VX.Game.Play {
                 Children = new Drawable[]
                 {
                     BtnEdit,
-                    BtnPlay
+                    BtnPlay,
+                    Storybooks = new TextFlowContainer(s => s.Font = new FontUsage("default", 30)),
                 }
             };
+
+            // Will create the Stories directory if it does not exist
+            if (!StoriesStorage.Exists("")) {
+                Directory.CreateDirectory("Stories");
+            }
+            var numStorybooks = StoriesStorage.GetDirectories("").Count();
+            Storybooks.Text = numStorybooks.ToString(CultureInfo.InvariantCulture);
         }
+
     }
 }
