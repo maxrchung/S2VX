0b80aea661654aeeff3cbfc4acccb258a020926e
Xenocidel <aliao4@uci.edu>
2020-10-13 23:02:43 -0700

    Detect and display all folders inside Stories

--- S2VX.Game/SongSelection/SongSelectionScreen.cs
+++ S2VX.Game/SongSelection/SongSelectionScreen.cs
@@ -3,21 +3,36 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.UserInterface;
+using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
 using osuTK;
 using S2VX.Game.Editor;
-using System.Globalization;
+using S2VX.Game.SongSelection.UserInterface;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace S2VX.Game.Play {
     public class SongSelectionScreen : Screen {
+        [Resolved]
+        private ScreenStack Screens { get; set; }
         private static Vector2 InputSize { get; } = new Vector2(100, 30);
 
-        private static NativeStorage StoriesStorage { get; } = new NativeStorage("Stories");
+        private static NativeStorage RootLevelStorage { get; } = new NativeStorage("Stories");
+        //private static StorageBackedResourceStore CurLevelResourceStore { get; } = new StorageBackedResourceStore(RootLevelStorage);
 
-        private TextFlowContainer Storybooks { get; set; }
+        private FillFlowContainer SelectionItems { get; set; }
+
+        private static List<Drawable> CreateSelectionItems() {
+            var selectionItems = new List<Drawable>();
+            var dirs = RootLevelStorage.GetDirectories("");
+            foreach (var dir in dirs) {
+                selectionItems.Add(new SelectedItemDisplay {
+                    Text = dir
+                });
+            }
+            return selectionItems;
+        }
 
         private Button BtnEdit { get; } = new BasicButton() {
             Size = InputSize,
@@ -31,24 +46,42 @@ namespace S2VX.Game.Play {
 
         [BackgroundDependencyLoader]
         private void Load() {
+            // Create the Stories directory if it does not exist
+            if (!RootLevelStorage.Exists("")) {
+                Directory.CreateDirectory("Stories");
+            }
+
             BtnEdit.Action = () => this.Push(new EditorScreen());
             BtnPlay.Action = () => this.Push(new PlayScreen(false));
+
+            SelectionItems = new FillFlowContainer {
+                RelativeSizeAxes = Axes.Both,
+                RelativePositionAxes = Axes.Both,
+                Width = 1.0f,
+                Height = 1.0f,
+                Direction = FillDirection.Full,
+                Children = CreateSelectionItems(),
+            };
+
             InternalChild = new FillFlowContainer {
-                Direction = FillDirection.Horizontal,
+                Width = Screens.DrawWidth,
+                Height = Screens.DrawHeight,
+                Direction = FillDirection.Full,
                 Children = new Drawable[]
                 {
                     BtnEdit,
                     BtnPlay,
-                    Storybooks = new TextFlowContainer(s => s.Font = new FontUsage("default", 30)),
+                    new BasicScrollContainer {
+                        Width = Screens.DrawWidth,
+                        Height = Screens.DrawHeight,
+                        Margin = new MarginPadding {
+                            Vertical = Width / 10,
+                            Horizontal = Width / 10
+                        },
+                        Child = SelectionItems
+                    },
                 }
             };
-
-            // Will create the Stories directory if it does not exist
-            if (!StoriesStorage.Exists("")) {
-                Directory.CreateDirectory("Stories");
-            }
-            var numStorybooks = StoriesStorage.GetDirectories("").Count();
-            Storybooks.Text = numStorybooks.ToString(CultureInfo.InvariantCulture);
         }
 
     }
---
+++ S2VX.Game/SongSelection/UserInterface/SelectedItemDisplay.cs
@@ -0,0 +1,57 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Screens;
+using osuTK.Graphics;
+
+namespace S2VX.Game.SongSelection.UserInterface {
+    public class SelectedItemDisplay : CompositeDrawable {
+        [Resolved]
+        private ScreenStack Screens { get; set; }
+
+        public bool IsSelected { get; set; }
+        public string Text { get; set; }
+        public RelativeBox SelectedIndicatorBox { get; private set; }
+        private RelativeBox ThumbnailBox { get; set; }
+        private RelativeBox TextShadowBox { get; set; }
+        private TextFlowContainer TextContainer { get; set; }
+
+        [BackgroundDependencyLoader]
+        private void Load() {
+            var boxSize = Screens.DrawWidth / 4;
+            Width = boxSize;
+            Height = boxSize;
+            Margin = new MarginPadding {
+                Vertical = boxSize / 10,
+                Horizontal = boxSize / 10
+            };
+            InternalChildren = new Drawable[] {
+                SelectedIndicatorBox = new RelativeBox {
+                    Width = 1.2f,
+                    Height = 1.2f,
+                    Alpha = IsSelected ? 1 : 0
+                },
+                ThumbnailBox = new RelativeBox {
+                    Colour = Color4.White.Opacity(0.9f),
+                },
+                TextShadowBox = new RelativeBox {
+                    Anchor = Anchor.BottomCentre,
+                    Origin = Anchor.BottomCentre,
+                    Height = 0.2f,
+                    Colour = Color4.Black.Opacity(0.5f),
+                },
+                TextContainer = new TextFlowContainer(s => s.Font = new FontUsage("default", Screens.DrawWidth / 40)) {
+                    RelativeSizeAxes = Axes.Both,
+                    RelativePositionAxes = Axes.Both,
+                    Y = 0.8f,
+                    Height = 0.2f,
+                    TextAnchor = Anchor.TopCentre,
+                    Text = Text,
+                },
+            };
+        }
+
+    }
+}
