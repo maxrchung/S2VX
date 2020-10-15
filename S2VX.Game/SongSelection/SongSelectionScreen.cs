67eeb29dc2e5aced20ba4c7bac3e01d069adcfd2
Xenocidel <aliao4@uci.edu>
2020-10-14 20:12:35 -0700

    Finish folder navigation

--- S2VX.Game/SongSelection/SongSelectionScreen.cs
+++ S2VX.Game/SongSelection/SongSelectionScreen.cs
@@ -2,12 +2,11 @@
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Graphics.UserInterface;
-using osu.Framework.IO.Stores;
+using osu.Framework.Input.Events;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
 using osuTK;
-using S2VX.Game.Editor;
+using osuTK.Input;
 using S2VX.Game.SongSelection.UserInterface;
 using System.Collections.Generic;
 using System.IO;
@@ -17,20 +16,24 @@ namespace S2VX.Game.Play {
     public class SongSelectionScreen : Screen {
         [Resolved]
         private ScreenStack Screens { get; set; }
-        private static Vector2 InputSize { get; } = new Vector2(100, 30);
 
-        private static NativeStorage RootLevelStorage { get; } = new NativeStorage("Stories");
+        public string CurSelectionPath { get; set; } = "Stories";
+
+        private TextFlowContainer CurSelectionPathTxt { get; set; }
+
+        public static NativeStorage Storage { get; set; }
         //private static StorageBackedResourceStore CurLevelResourceStore { get; } = new StorageBackedResourceStore(RootLevelStorage);
 
         private FillFlowContainer SelectionItems { get; set; }
 
-        private static List<Drawable> CreateSelectionItems() {
+        private List<Drawable> CreateSelectionItems() {
             var selectionItems = new List<Drawable>();
-            var dirs = RootLevelStorage.GetDirectories("");
+            var dirs = Storage.GetDirectories("");
             foreach (var dir in dirs) {
                 if (DirectoryContainsStory(dir) || DirectoryContainsDirectories(dir)) {
                     selectionItems.Add(new SelectedItemDisplay {
-                        Text = dir
+                        ItemName = dir,
+                        SongSelectionScreen = this,
                     });
                 }
             }
@@ -38,32 +41,39 @@ namespace S2VX.Game.Play {
         }
 
         private static bool DirectoryContainsStory(string dir) {
-            var story = RootLevelStorage.GetFiles(dir, "story.json");
+            var story = Storage.GetFiles(dir, "story.json");
             //var song = RootLevelStorage.GetFiles(dir, "*.mp3");
             return story.Count() == 1;
         }
 
-        private static bool DirectoryContainsDirectories(string dir) => RootLevelStorage.GetDirectories(dir).Any();
+        private static bool DirectoryContainsDirectories(string dir) => Storage.GetDirectories(dir).Any();
 
-        private Button BtnEdit { get; } = new BasicButton() {
-            Size = InputSize,
-            Text = "Edit"
-        };
-
-        private Button BtnPlay { get; } = new BasicButton() {
-            Size = InputSize,
-            Text = "Play"
-        };
+        protected override bool OnKeyDown(KeyDownEvent e) {
+            switch (e.Key) {
+                case Key.Escape:
+                    // Up one level, unless we're already at root level
+                    if (CurSelectionPath != "Stories") {
+                        this.Exit();
+                        return true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
 
         [BackgroundDependencyLoader]
         private void Load() {
-            // Create the Stories directory if it does not exist
-            if (!RootLevelStorage.Exists("")) {
-                Directory.CreateDirectory("Stories");
+            Storage = new NativeStorage(CurSelectionPath);
+            // Create the Stories root level directory if it does not exist
+            if (CurSelectionPath == "Stories" && !Storage.Exists("")) {
+                Directory.CreateDirectory(CurSelectionPath);
             }
 
-            BtnEdit.Action = () => this.Push(new EditorScreen());
-            BtnPlay.Action = () => this.Push(new PlayScreen(false));
+            var fullWidth = Screens.DrawWidth;
+            var fullHeight = Screens.DrawHeight;
+            var titleSize = fullHeight / 30;
 
             SelectionItems = new FillFlowContainer {
                 RelativeSizeAxes = Axes.Both,
@@ -75,13 +85,16 @@ namespace S2VX.Game.Play {
             };
 
             InternalChild = new FillFlowContainer {
-                Width = Screens.DrawWidth,
-                Height = Screens.DrawHeight,
+                Width = fullWidth,
+                Height = fullHeight,
                 Direction = FillDirection.Full,
                 Children = new Drawable[]
                 {
-                    BtnEdit,
-                    BtnPlay,
+                    CurSelectionPathTxt = new TextFlowContainer(s => s.Font = new FontUsage("default", titleSize)) {
+                        Width = fullWidth,
+                        Height = titleSize,
+                        Text = CurSelectionPath,
+                    },
                     new BasicScrollContainer {
                         Width = Screens.DrawWidth,
                         Height = Screens.DrawHeight,
--- S2VX.Game/SongSelection/UserInterface/SelectedItemDisplay.cs
+++ S2VX.Game/SongSelection/UserInterface/SelectedItemDisplay.cs
@@ -3,38 +3,91 @@ using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input.Events;
 using osu.Framework.Screens;
+using osuTK;
 using osuTK.Graphics;
+using S2VX.Game.Editor;
+using S2VX.Game.Play;
 
 namespace S2VX.Game.SongSelection.UserInterface {
     public class SelectedItemDisplay : CompositeDrawable {
         [Resolved]
         private ScreenStack Screens { get; set; }
 
-        public bool IsSelected { get; set; }
-        public string Text { get; set; }
+
+        public SongSelectionScreen SongSelectionScreen { get; set; }    // Set in SongSelectionScreen
+        public string ItemName { get; set; }                            // Set in SongSelectionScreen
         public RelativeBox SelectedIndicatorBox { get; private set; }
         private RelativeBox ThumbnailBox { get; set; }
         private RelativeBox TextShadowBox { get; set; }
         private TextFlowContainer TextContainer { get; set; }
+        private Button BtnEdit { get; set; }
+        private Button BtnPlay { get; set; }
+
+
+        protected override bool OnHover(HoverEvent e) {
+            SelectedIndicatorBox.Alpha = 1;
+            ThumbnailBox.Alpha = 0.7f;
+            BtnEdit.Alpha = 1;
+            BtnPlay.Alpha = 1;
+            return false;
+        }
+
+        protected override void OnHoverLost(HoverLostEvent e) {
+            SelectedIndicatorBox.Alpha = 0;
+            ThumbnailBox.Alpha = 1;
+            BtnEdit.Alpha = 0;
+            BtnPlay.Alpha = 0;
+        }
+
+        protected override bool OnClick(ClickEvent e) {
+            Screens.Push(new SongSelectionScreen {
+                CurSelectionPath = SongSelectionScreen.CurSelectionPath + "/" + ItemName,
+            });
+            return true;
+        }
 
         [BackgroundDependencyLoader]
         private void Load() {
             var boxSize = Screens.DrawWidth / 4;
+            var textSize = Screens.DrawWidth / 40;
+            var btnSize = new Vector2(Screens.DrawWidth / 10, Screens.DrawWidth / 30);
+
+            BtnEdit = new BasicButton {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Size = btnSize,
+                Text = "Edit",
+                Alpha = 0,
+                Action = () => SongSelectionScreen.Push(new EditorScreen()),
+            };
+            BtnPlay = new BasicButton {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                Size = btnSize,
+                Text = "Play",
+                Alpha = 0,
+                Action = () => SongSelectionScreen.Push(new PlayScreen(false)),
+            };
+
             Width = boxSize;
             Height = boxSize;
             Margin = new MarginPadding {
                 Vertical = boxSize / 10,
                 Horizontal = boxSize / 10
             };
+
             InternalChildren = new Drawable[] {
                 SelectedIndicatorBox = new RelativeBox {
-                    Width = 1.2f,
-                    Height = 1.2f,
-                    Alpha = IsSelected ? 1 : 0
+                    Width = 1.1f,
+                    Height = 1.1f,
+                    Alpha = 0
                 },
                 ThumbnailBox = new RelativeBox {
-                    Colour = Color4.White.Opacity(0.9f),
+                    // Red box as placeholder
+                    Colour = Color4.Red,
                 },
                 TextShadowBox = new RelativeBox {
                     Anchor = Anchor.BottomCentre,
@@ -42,14 +95,23 @@ namespace S2VX.Game.SongSelection.UserInterface {
                     Height = 0.2f,
                     Colour = Color4.Black.Opacity(0.5f),
                 },
-                TextContainer = new TextFlowContainer(s => s.Font = new FontUsage("default", Screens.DrawWidth / 40)) {
+                TextContainer = new TextFlowContainer(s => s.Font = new FontUsage("default", textSize)) {
                     RelativeSizeAxes = Axes.Both,
                     RelativePositionAxes = Axes.Both,
                     Y = 0.8f,
                     Height = 0.2f,
                     TextAnchor = Anchor.TopCentre,
-                    Text = Text,
+                    Text = ItemName,
                 },
+                new FillFlowContainer {
+                    Anchor = Anchor.Centre,
+                    Origin = Anchor.Centre,
+                    Direction = FillDirection.Horizontal,
+                    Children = new Drawable[] {
+                        BtnEdit,
+                        BtnPlay,
+                    }
+                }
             };
         }
 
