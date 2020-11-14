6ce6828af126d5d89d9764912c31ffca44ba95e6
Naranja-Sagged <alexliuying@gmail.com>
2020-11-14 03:29:44 -0800

    Hold approaches not appearing fixed

--- S2VX.Game/Story/Note/Approaches.cs
+++ S2VX.Game/Story/Note/Approaches.cs
@@ -49,64 +49,72 @@ namespace S2VX.Game.Story.Note {
         [BackgroundDependencyLoader]
         private void Load() => RelativeSizeAxes = Axes.Both;
 
+        private void UpdateApproach(Approach approach, double approachTime, List<RelativeBox> approachLines) {
+            var notes = Story.Notes;
+            var camera = Story.Camera;
+            var approaches = Story.Approaches;
+
+            var time = Time.Current;
+            var endFadeOut = approachTime + notes.FadeOutTime;
+
+            if (time >= endFadeOut) {
+                approachLines.ForEach(l => l.Alpha = 0); // Lines vs ReleaseLines
+                // Return early to save some calculations
+                return;
+            }
+
+            var startTime = approachTime - notes.ShowTime;
+            var startFadeIn = startTime - notes.FadeInTime;
+
+            var position = camera.Position;
+            var rotation = camera.Rotation;
+            var scale = camera.Scale;
+            var thickness = approaches.Thickness;
+
+            var offset = S2VXUtils.Rotate(approach.Coordinates - position, rotation) * scale;
+
+            var distance = time < approachTime
+                ? Interpolation.ValueAt(time, approaches.Distance, scale.X / 2, startFadeIn, approachTime)
+                : scale.X / 2;
+            var rotationX = S2VXUtils.Rotate(new Vector2(distance, 0), rotation);
+            var rotationY = S2VXUtils.Rotate(new Vector2(0, distance), rotation);
+
+            // Add extra thickness so corners overlap
+            var overlap = distance * 2 + thickness;
+
+            approachLines[0].Position = offset + rotationY;
+            approachLines[0].Rotation = rotation;
+            approachLines[0].Size = new Vector2(overlap, thickness);
+
+            approachLines[1].Position = offset - rotationY;
+            approachLines[1].Rotation = rotation;
+            approachLines[1].Size = new Vector2(overlap, thickness);
+
+            approachLines[2].Position = offset + rotationX;
+            approachLines[2].Rotation = rotation;
+            approachLines[2].Size = new Vector2(thickness, overlap);
+
+            approachLines[3].Position = offset - rotationX;
+            approachLines[3].Rotation = rotation;
+            approachLines[3].Size = new Vector2(thickness, overlap);
+
+            float alpha;
+            if (time >= approachTime) {
+                alpha = Interpolation.ValueAt(time, 1.0f, 0.0f, approachTime, endFadeOut);
+            } else if (time >= startTime) {
+                alpha = 1;
+            } else {
+                alpha = Interpolation.ValueAt(time, 0.0f, 1.0f, startFadeIn, startTime);
+            }
+            approachLines.ForEach(l => l.Alpha = alpha);
+        }
+
         protected override void Update() {
             foreach (var approach in Children) {
-                var notes = Story.Notes;
-                var camera = Story.Camera;
-                var approaches = Story.Approaches;
-
-                var time = Time.Current;
-                var endFadeOut = approach.HitTime + notes.FadeOutTime;
-
-                if (time >= endFadeOut) {
-                    approach.Alpha = 0;
-                    // Continue early to save some calculations
-                    continue;
-                }
-
-                var startTime = approach.HitTime - notes.ShowTime;
-                var startFadeIn = startTime - notes.FadeInTime;
-
-                var position = camera.Position;
-                var rotation = camera.Rotation;
-                var scale = camera.Scale;
-                var thickness = approaches.Thickness;
-
-                var offset = S2VXUtils.Rotate(approach.Coordinates - position, rotation) * scale;
-
-                var distance = time < approach.HitTime
-                    ? Interpolation.ValueAt(time, approaches.Distance, scale.X / 2, startFadeIn, approach.HitTime)
-                    : scale.X / 2;
-                var rotationX = S2VXUtils.Rotate(new Vector2(distance, 0), rotation);
-                var rotationY = S2VXUtils.Rotate(new Vector2(0, distance), rotation);
-
-                // Add extra thickness so corners overlap
-                var overlap = distance * 2 + thickness;
-
-                approach.Lines[0].Position = offset + rotationY;
-                approach.Lines[0].Rotation = rotation;
-                approach.Lines[0].Size = new Vector2(overlap, thickness);
-
-                approach.Lines[1].Position = offset - rotationY;
-                approach.Lines[1].Rotation = rotation;
-                approach.Lines[1].Size = new Vector2(overlap, thickness);
-
-                approach.Lines[2].Position = offset + rotationX;
-                approach.Lines[2].Rotation = rotation;
-                approach.Lines[2].Size = new Vector2(thickness, overlap);
-
-                approach.Lines[3].Position = offset - rotationX;
-                approach.Lines[3].Rotation = rotation;
-                approach.Lines[3].Size = new Vector2(thickness, overlap);
-
-                if (time >= approach.HitTime) {
-                    var alpha = Interpolation.ValueAt(time, 1.0f, 0.0f, approach.HitTime, endFadeOut);
-                    approach.Alpha = alpha;
-                } else if (time >= startTime) {
-                    approach.Alpha = 1;
-                } else {
-                    var alpha = Interpolation.ValueAt(time, 0.0f, 1.0f, startFadeIn, startTime);
-                    approach.Alpha = alpha;
+                UpdateApproach(approach, approach.HitTime, approach.Lines);
+                // Update release approach if this is a Hold Note
+                if (approach is HoldApproach holdApproach) {
+                    UpdateApproach(holdApproach, holdApproach.EndTime, holdApproach.ReleaseLines);
                 }
             }
         }
