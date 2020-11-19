a3755e2389be791c46638a10eb8194da25675ed9
Xenocidel <aliao4@uci.edu>
2020-11-18 19:41:25 -0800

    lmao forgot to git add

---
+++ S2VX.Game/Editor/Reversible/ReversibleRemoveHoldNote.cs
@@ -0,0 +1,32 @@
+﻿using S2VX.Game.Editor.ToolState;
+using S2VX.Game.Story;
+using S2VX.Game.Story.Note;
+
+namespace S2VX.Game.Editor.Reversible {
+    public class ReversibleRemoveHoldNote : IReversible {
+        private EditorScreen Editor { get; set; }
+        private S2VXStory Story { get; set; }
+        private HoldNote Note { get; }
+
+        public ReversibleRemoveHoldNote(S2VXStory story, HoldNote note, EditorScreen editor) {
+            Story = story;
+            Note = note;
+            Editor = editor;
+        }
+
+        public void Undo() {
+            Story.AddHoldNote(Note);
+            if (Editor.ToolState is SelectToolState selectTool) {
+                selectTool.ClearNoteSelection();
+                selectTool.AddNoteSelection(Note);
+            }
+        }
+
+        public void Redo() {
+            Story.RemoveNote(Note);
+            if (Editor.ToolState is SelectToolState selectTool) {
+                selectTool.ClearNoteSelection();
+            }
+        }
+    }
+}
