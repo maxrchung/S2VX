b64d5f3667e5ba216686ff73972dea6aca280f39
Xenocidel <aliao4@uci.edu>
2020-12-01 20:38:12 -0800

    Add HoldNotes Color and Outline Commands

---
+++ S2VX.Game/Story/Command/HoldNotesOutlineColorCommand.cs
@@ -0,0 +1,21 @@
+﻿using osu.Framework.Utils;
+using osuTK.Graphics;
+
+namespace S2VX.Game.Story.Command {
+    public class HoldNotesOutlineColorCommand : S2VXCommand {
+        public Color4 StartValue { get; set; } = Color4.Red;
+        public Color4 EndValue { get; set; } = Color4.Red;
+        public override void Apply(double time, S2VXStory story) {
+            var value = Interpolation.ValueAt(time, StartValue, EndValue, StartTime, EndTime, Easing);
+            story.Notes.GetHoldNotes().ForEach(note => note.OutlineColor = value);
+        }
+        protected override string ToValues() => $"{S2VXUtils.Color4ToString(StartValue)}|{S2VXUtils.Color4ToString(EndValue)}";
+        public static NotesOutlineColorCommand FromString(string[] split) {
+            var command = new NotesOutlineColorCommand() {
+                StartValue = S2VXUtils.Color4FromString(split[4]),
+                EndValue = S2VXUtils.Color4FromString(split[5]),
+            };
+            return command;
+        }
+    }
+}
---
+++ S2VX.Game/Story/Command/HoldNotesOutlineThicknessCommand.cs
@@ -0,0 +1,21 @@
+﻿using osu.Framework.Utils;
+using System.Globalization;
+
+namespace S2VX.Game.Story.Command {
+    public class HoldNotesOutlineThicknessCommand : S2VXCommand {
+        public float StartValue { get; set; } = 0.005f;
+        public float EndValue { get; set; } = 0.005f;
+        public override void Apply(double time, S2VXStory story) {
+            var value = Interpolation.ValueAt(time, StartValue, EndValue, StartTime, EndTime, Easing);
+            story.Notes.GetHoldNotes().ForEach(note => note.OutlineThickness = value);
+        }
+        protected override string ToValues() => $"{StartValue}|{EndValue}";
+        public static NotesOutlineThicknessCommand FromString(string[] split) {
+            var command = new NotesOutlineThicknessCommand() {
+                StartValue = float.Parse(split[4], CultureInfo.InvariantCulture),
+                EndValue = float.Parse(split[5], CultureInfo.InvariantCulture),
+            };
+            return command;
+        }
+    }
+}
