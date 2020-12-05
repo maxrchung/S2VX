3662968266603414734a746431fcfda98f98cee2
Xenocidel <aliao4@uci.edu>
2020-12-04 19:56:37 -0800

    Separate HoldApproachColor from ApproachColor; change default template style

--- S2VX.Game/Story/Command/HoldApproachesColorCommand.cs
+++ S2VX.Game/Story/Command/HoldApproachesColorCommand.cs
@@ -2,16 +2,16 @@
 using osuTK.Graphics;
 
 namespace S2VX.Game.Story.Command {
-    public class ApproachesColorCommand : S2VXCommand {
+    public class HoldApproachesColorCommand : S2VXCommand {
         public Color4 StartValue { get; set; } = Color4.Blue;
         public Color4 EndValue { get; set; } = Color4.Blue;
         public override void Apply(double time, S2VXStory story) {
             var value = Interpolation.ValueAt(time, StartValue, EndValue, StartTime, EndTime, Easing);
-            story.Notes.GetNonHoldNotes().ForEach(note => note.Approach.Colour = value);
+            story.Notes.GetHoldNotes().ForEach(note => note.Approach.Colour = value);
         }
         protected override string ToValues() => $"{S2VXUtils.Color4ToString(StartValue)}|{S2VXUtils.Color4ToString(EndValue)}";
-        public static ApproachesColorCommand FromString(string[] split) {
-            var command = new ApproachesColorCommand() {
+        public static HoldApproachesColorCommand FromString(string[] split) {
+            var command = new HoldApproachesColorCommand() {
                 StartValue = S2VXUtils.Color4FromString(split[4]),
                 EndValue = S2VXUtils.Color4FromString(split[5]),
             };
