2950ef7a47899109c50493f64f34d514a03bac1d
maxrchung <maxrchung@gmail.com>
2020-12-15 20:26:54 -0800

    Add basic leave screen

---
+++ S2VX.Game/LeaveScreen.cs
@@ -0,0 +1,33 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Screens;
+using osuTK;
+
+namespace S2VX.Game {
+    public class LeaveScreen : Screen {
+        private Vector2 ButtonSize { get; set; } = new Vector2(100, 30);
+
+        [BackgroundDependencyLoader]
+        private void Load() =>
+            InternalChildren = new Drawable[] {
+                new FillFlowContainer() {
+                    new SpriteText() {
+                    Text = "Are you sure you want to leave?"
+                },
+                    new BasicButton() {
+                        Text = "OK",
+                        Action = () => this.GetParentScreen().GetParentScreen().MakeCurrent(),
+                        Size = ButtonSize
+                    },
+                    new BasicButton() {
+                        Text = "Cancel",
+                        Action = () => this.Exit(),
+                        Size = ButtonSize
+                    }
+                }
+            };
+    }
+}
