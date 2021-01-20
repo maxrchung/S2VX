ad6700835fffc0d4ba1b13779f7746462256c22c
maxrchung <maxrchung@gmail.com>
2021-01-19 22:29:52 -0800

    Remove unused using statement

--- S2VX.Game/Story/Command/S2VXCommand.cs
+++ S2VX.Game/Story/Command/S2VXCommand.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Linq;
 using osu.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 
