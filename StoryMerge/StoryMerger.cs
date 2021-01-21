2d4f133b799da3fbe7730e349446f1f5e217af37
maxrchung <maxrchung@gmail.com>
2021-01-21 02:49:32 -0800

    Update StoryMerger to have more exposed functions to test

--- StoryMerge/StoryMerger.cs
+++ StoryMerge/StoryMerger.cs
@@ -1,42 +1,47 @@
-﻿using System.IO;
+﻿using S2VX.Game.Story;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace StoryMerge {
     public class StoryMerger {
-        private string[] Inputs { get; }
-        private string Output { get; }
+        private string[] Inputs { get; set; }
+        private string Output { get; set; }
 
         public StoryMerger(string[] inputs, string output) {
             Inputs = inputs;
             Output = output;
         }
 
-        public async Task<MergeResult> Merge() {
-            var validation = ValidateParameters();
-            if (!validation.IsSuccessful) {
-                return validation;
+        public async Task<Result> Merge() {
+            var result = ValidateParameters();
+            if (!result.IsSuccessful) {
+                return result;
             }
 
+            result = ValidateInputs();
+            if (!result.IsSuccessful) {
+                return result;
+            }
 
             await File.WriteAllTextAsync(Output, "");
-            return new MergeResult {
+            return new Result {
                 IsSuccessful = true,
                 Message = $"Merged {string.Join(',', Inputs)} to {Output}"
             };
         }
 
-        private MergeResult ValidateParameters() {
-            var result = new MergeResult { IsSuccessful = true };
-
-            if (Inputs == null || Inputs.Length <= 2) {
-                result = new MergeResult {
+        public Result ValidateParameters() {
+            if (Inputs == null || Inputs.Length < 2) {
+                return new Result {
                     IsSuccessful = false,
                     Message = "2 or more inputs must be provided"
                 };
             }
 
-            if (Output == null) {
-                result = new MergeResult {
+            if (string.IsNullOrWhiteSpace(Output)) {
+                return new Result {
                     IsSuccessful = false,
                     Message = "1 output must be provided"
                 };
@@ -44,21 +49,31 @@ namespace StoryMerge {
 
             foreach (var path in Inputs) {
                 if (!File.Exists(path)) {
-                    result = new MergeResult {
+                    return new Result {
                         IsSuccessful = false,
                         Message = $"Input file does not exist: {path}"
                     };
                 }
             }
 
-            if (!File.Exists(Output)) {
-                result = new MergeResult {
-                    IsSuccessful = false,
-                    Message = $"Output file does not exist: {Output}"
-                };
+            return new Result { IsSuccessful = true };
+        }
+
+        [SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>")]
+        public Result ValidateInputs() {
+            foreach (var input in Inputs) {
+                var story = new S2VXStory();
+                try {
+                    story.Open(input, false);
+                } catch (Exception e) {
+                    return new Result {
+                        IsSuccessful = false,
+                        Message = $"Failed to load: {input}\n{e.Message}"
+                    };
+                }
             }
 
-            return result;
+            return new Result { IsSuccessful = true };
         }
     }
 }
