820d0f881d7ecbf6d526cf8a592126db1f372dd7
maxrchung <maxrchung@gmail.com>
2021-01-27 23:08:33 -0800

    Add S2VXTestScene to add SquareContainer constraint

---
+++ S2VX.Game.Tests/S2VXTestScene.cs
@@ -0,0 +1,11 @@
+﻿using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Testing;
+
+namespace S2VX.Game.Tests {
+    public class S2VXTestScene : TestScene {
+        protected override Container<Drawable> Content { get; }
+
+        protected S2VXTestScene() => base.Content.Add(Content = new SquareContainer());
+    }
+}
