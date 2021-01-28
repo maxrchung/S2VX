fa9231770de5d40a62fe6d9dd9531acad521cf62
maxrchung <maxrchung@gmail.com>
2021-01-27 23:12:29 -0800

    Update namespaces to match folder structure

--- S2VX.Game.Tests/VisualTests/StoryTests.cs
+++ S2VX.Game.Tests/VisualTests/StoryTests.cs
@@ -1,7 +1,7 @@
 using osu.Framework.Allocation;
 using S2VX.Game.Story;
 
-namespace S2VX.Game.Tests.Visual {
+namespace S2VX.Game.Tests.VisualTests {
     public class StoryTests : S2VXTestScene {
         // Add visual tests to ensure correct behaviour of your game: https://github.com/ppy/osu-framework/wiki/Development-and-Testing
         // You can make changes to classes associated with the tests and they will recompile and update immediately.
