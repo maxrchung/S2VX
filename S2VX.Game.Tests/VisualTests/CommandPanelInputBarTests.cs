7f5fa475163138bf3b96bdf3a8c3fb8c6d8119dd
maxrchung <maxrchung@gmail.com>
2021-02-02 01:12:18 -0800

    Capatilize step description for consistency

--- S2VX.Game.Tests/VisualTests/CommandPanelInputBarTests.cs
+++ S2VX.Game.Tests/VisualTests/CommandPanelInputBarTests.cs
@@ -14,44 +14,44 @@ namespace S2VX.Game.Tests.VisualTests {
         private CommandPanelInputBar InputBar { get; set; }
 
         private void CreateAddInputBar() =>
-            AddStep("create add input bar", () => Add(InputBar = CommandPanelInputBar.CreateAddInputBar(ValueChangedHandler, EmptyHandler)));
+            AddStep("Create add input bar", () => Add(InputBar = CommandPanelInputBar.CreateAddInputBar(ValueChangedHandler, EmptyHandler)));
 
         private void CreateEditInputBar() =>
-            AddStep("create edit input bar", () => Add(InputBar = CommandPanelInputBar.CreateEditInputBar(EmptyHandler)));
+            AddStep("Create edit input bar", () => Add(InputBar = CommandPanelInputBar.CreateEditInputBar(EmptyHandler)));
 
         [Test]
         public void Tab_FocusOnFirstInput_ShiftsFocusToNextInput() {
             CreateAddInputBar();
-            AddStep("focus start time input", () => InputManager.ChangeFocus(InputBar.TxtStartTime));
-            AddStep("press tab", () => InputManager.PressKey(Key.Tab));
-            AddAssert("shifts focus to next input", () => InputBar.TxtEndTime.HasFocus);
+            AddStep("Focus start time input", () => InputManager.ChangeFocus(InputBar.TxtStartTime));
+            AddStep("Press tab", () => InputManager.PressKey(Key.Tab));
+            AddAssert("Shifts focus to next input", () => InputBar.TxtEndTime.HasFocus);
         }
 
         [Test]
         public void ShiftTab_FocusOnFirstInput_ShiftsFocusToLastInput() {
             CreateAddInputBar();
-            AddStep("focus start time input", () => InputManager.ChangeFocus(InputBar.TxtStartTime));
-            AddStep("press shift tab", () => {
+            AddStep("Focus start time input", () => InputManager.ChangeFocus(InputBar.TxtStartTime));
+            AddStep("Press shift tab", () => {
                 InputManager.PressKey(Key.LShift);
                 InputManager.PressKey(Key.Tab);
             });
-            AddAssert("shifts focus to last input", () => InputBar.TxtEndValue.HasFocus);
+            AddAssert("Shifts focus to last input", () => InputBar.TxtEndValue.HasFocus);
         }
 
         [Test]
         public void SaveIcon_AddInputBar_IsPlus() {
             CreateAddInputBar();
             IconButton iconButton = null;
-            AddStep("set icon button", () => iconButton = InputBar.BtnSave as IconButton);
-            AddAssert("is plus", () => iconButton.Icon.Equals(FontAwesome.Solid.Plus));
+            AddStep("Get icon button", () => iconButton = InputBar.BtnSave as IconButton);
+            AddAssert("Is plus", () => iconButton.Icon.Equals(FontAwesome.Solid.Plus));
         }
 
         [Test]
         public void SaveIcon_EditInputBar_IsSave() {
             CreateEditInputBar();
             IconButton iconButton = null;
-            AddStep("set icon button", () => iconButton = InputBar.BtnSave as IconButton);
-            AddAssert("is save", () => iconButton.Icon.Equals(FontAwesome.Solid.Save));
+            AddStep("Get icon button", () => iconButton = InputBar.BtnSave as IconButton);
+            AddAssert("Is save", () => iconButton.Icon.Equals(FontAwesome.Solid.Save));
         }
     }
 }
