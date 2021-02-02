59695892ab7b2f8e23c19ca5d20342208874e3c8
maxrchung <maxrchung@gmail.com>
2021-02-01 21:44:38 -0800

    Wrote tests for command panel input bar

--- S2VX.Game.Tests/S2VXTestScene.cs
+++ S2VX.Game.Tests/S2VXTestScene.cs
@@ -3,7 +3,7 @@ using osu.Framework.Graphics.Containers;
 using osu.Framework.Testing;
 
 namespace S2VX.Game.Tests {
-    public class S2VXTestScene : TestScene {
+    public class S2VXTestScene : ManualInputManagerTestScene {
         protected override Container<Drawable> Content { get; }
 
         public S2VXTestScene() => base.Content.Add(Content = new SquareContainer());
