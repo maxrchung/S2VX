b847f9d8dc758b75ae3181168e28417222ab41c2
maxrchung <maxrchung@gmail.com>
2021-02-11 04:18:55 -0800

    Add screen stack management to tests

--- S2VX.Game.Tests/VisualTests/LoadStoryTests.cs
+++ S2VX.Game.Tests/VisualTests/LoadStoryTests.cs
@@ -1,61 +1,72 @@
 ﻿using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Screens;
 using S2VX.Game.Editor;
 using S2VX.Game.Play;
 using System.IO;
 
 namespace S2VX.Game.Tests.VisualTests {
     public class LoadStoryTests : S2VXTestScene {
-        private static string StoryDirectory { get; } = Path.Combine("VisualTests", "LoadTrackTests");
+        [Cached]
+        private ScreenStack ScreenStack { get; set; } = new ScreenStack();
+
+        [BackgroundDependencyLoader]
+        private void Load() =>
+            Add(ScreenStack);
+
+        private static string StoryDirectory { get; } = Path.Combine("VisualTests", "LoadStoryTests");
         private static string AudioPath { get; } = Path.Combine(StoryDirectory, "1-second-of-silence.mp3");
 
-        private void AddEditorScreen(string storyFileName) {
+        private void TestEditorScreen(string storyFileName) {
             var storyPath = Path.Combine(StoryDirectory, storyFileName);
-            AddStep("Add editor screen", () => Add(new EditorScreen(storyPath, AudioPath)));
+            AddStep("Add editor screen", () => ScreenStack.Push(new EditorScreen(storyPath, AudioPath)));
+            AddStep("Exit screen", () => ScreenStack.Exit());
         }
 
-        private void AddPlayScreen(string storyFileName) {
+        private void TestPlayScreen(string storyFileName) {
             var storyPath = Path.Combine(StoryDirectory, storyFileName);
-            AddStep("Add editor screen", () => Add(new PlayScreen(false, storyPath, AudioPath)));
+            AddStep("Add editor screen", () => ScreenStack.Push(new PlayScreen(false, storyPath, AudioPath)));
+            AddStep("Exit screen", () => ScreenStack.Exit());
         }
 
         [Test]
         public void LoadEditor_ValidStory_HasNoUnhandledException() =>
-            AddEditorScreen("ValidStory.s2ry");
+            TestEditorScreen("ValidStory.s2ry");
 
         [Test]
         public void LoadEditor_EmptyFile_HasNoUnhandledException() =>
-            AddEditorScreen("EmptyFile.s2ry");
+            TestEditorScreen("EmptyFile.s2ry");
 
         [Test]
         public void LoadEditor_MalformedJson_HasNoUnhandledException() =>
-            AddEditorScreen("MalformedJSON.s2ry");
+            TestEditorScreen("MalformedJSON.s2ry");
 
         [Test]
         public void LoadEditor_MissingHoldNotesProperty_HasNoUnhandledException() =>
-            AddEditorScreen("MissingHoldNotesProperty.s2ry");
+            TestEditorScreen("MissingHoldNotesProperty.s2ry");
 
         [Test]
         public void LoadEditor_MissingEditorSettings_HasNoUnhandledException() =>
-            AddEditorScreen("MissingEditorSettings.s2ry");
+            TestEditorScreen("MissingEditorSettings.s2ry");
 
         [Test]
         public void LoadPlay_ValidStory_HasNoUnhandledException() =>
-            AddPlayScreen("ValidStory.s2ry");
+            TestPlayScreen("ValidStory.s2ry");
 
         [Test]
         public void LoadPlay_EmptyFile_HasNoUnhandledException() =>
-            AddPlayScreen("EmptyFile.s2ry");
+            TestPlayScreen("EmptyFile.s2ry");
 
         [Test]
         public void LoadPlay_MalformedJson_HasNoUnhandledException() =>
-            AddPlayScreen("MalformedJSON.s2ry");
+            TestPlayScreen("MalformedJSON.s2ry");
 
         [Test]
         public void LoadPlay_MissingHoldNotesProperty_HasNoUnhandledException() =>
-            AddPlayScreen("MissingHoldNotesProperty.s2ry");
+            TestPlayScreen("MissingHoldNotesProperty.s2ry");
 
         [Test]
         public void LoadPlay_MissingEditorSettings_HasNoUnhandledException() =>
-            AddPlayScreen("MissingEditorSettings.s2ry");
+            TestPlayScreen("MissingEditorSettings.s2ry");
     }
 }
