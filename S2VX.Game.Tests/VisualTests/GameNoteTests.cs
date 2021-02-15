0df774da604dc9f25cc4ee12fc189bde03e93cdf
Naranja-Sagged <alexliuying@gmail.com>
2021-02-15 15:28:14 -0800

    start tests

---
+++ S2VX.Game.Tests/VisualTests/GameNoteTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using osu.Framework.Allocation;
+using S2VX.Game.Story;
+using S2VX.Game.Story.Note;
+
+namespace S2VX.Game.Tests.VisualTests {
+    public class GameNoteTests : S2VXTestScene {
+        [Cached]
+        private S2VXStory Story { get; set; } = new S2VXStory();
+
+        [BackgroundDependencyLoader]
+        private void Load() =>
+            Add(Story);
+
+        [Test]
+        public void OnClick_StackedNotes_HitsTopNote() {
+            GameNote gameNote1 = null;
+            AddStep("Add game note", () => Story.AddNote(gameNote1 = new GameNote {
+                HitTime = Time.Current
+            }));
+            AddStep("Add game note", () => Story.AddNote(new GameNote {
+                HitTime = Time.Current + 1
+            }));
+            AddStep("Add game note", () => Story.AddNote(new GameNote {
+                HitTime = Time.Current + 2
+            }));
+            AddStep("move mouse to centre", () => InputManager.MoveMouseTo(gameNote1.Position));
+        }
+    }
+}
