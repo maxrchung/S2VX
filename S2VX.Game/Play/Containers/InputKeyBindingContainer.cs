e0f53aff312f92452e9efa0d190c4af0d941fa4b
maxrchung <maxrchung@gmail.com>
2021-02-18 02:13:14 -0800

    Correctly specify key bindings

--- S2VX.Game/Play/Containers/InputKeyBindingContainer.cs
+++ S2VX.Game/Play/Containers/InputKeyBindingContainer.cs
@@ -5,22 +5,18 @@ using System.Collections.Generic;
 namespace S2VX.Game.Story.Play.Containers {
     public class InputKeyBindingContainer : KeyBindingContainer<InputAction> {
         public InputKeyBindingContainer() : base(SimultaneousBindingMode.All) { }
+
         public override IEnumerable<IKeyBinding> DefaultKeyBindings => new[] {
-            new KeyBinding(
-                new[] {
-                    InputKey.Z,
-                    InputKey.X,
-                    InputKey.C,
-                    InputKey.V,
-                    InputKey.A,
-                    InputKey.S,
-                    InputKey.D,
-                    InputKey.F,
-                    InputKey.MouseLeft,
-                    InputKey.MouseRight
-                },
-                InputAction.Input
-            ),
+            new KeyBinding(new[] { InputKey.Z }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.X }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.C }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.V }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.A }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.S }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.D }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.F }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.MouseLeft }, InputAction.Input),
+            new KeyBinding(new[] { InputKey.MouseRight }, InputAction.Input)
         };
     }
 }
