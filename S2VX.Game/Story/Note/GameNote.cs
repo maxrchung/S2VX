97ac48ac9706cd4df3143dfb2a5f91ed91f48a41
Naranja-Sagged <alexliuying@gmail.com>
2021-02-22 20:40:47 -0800

    Remove comments and prints

--- S2VX.Game/Story/Note/GameNote.cs
+++ S2VX.Game/Story/Note/GameNote.cs
@@ -6,7 +6,6 @@ using osuTK.Graphics;
 using S2VX.Game.Play;
 using S2VX.Game.Play.UserInterface;
 using System;
-using System.Linq;
 
 namespace S2VX.Game.Story.Note {
     public class GameNote : S2VXNote, IKeyBindingHandler<InputAction> {
@@ -49,7 +48,7 @@ namespace S2VX.Game.Story.Note {
             FlagForRemoval();
         }
 
-        // Notes are clickable if they are visible on screen, not missed, and is the earliest note
+        // Notes are clickable if they are hovered, not missed, and is the earliest note
         private bool IsClickable() {
             if (Story.Notes.HasClickedNote) {
                 return false;
@@ -59,26 +58,8 @@ namespace S2VX.Game.Story.Note {
             // Limit timing error to be +/- MissThreshold (though it will never be >= MissThreshold since RecordMiss would have already run)
             TimingError = (int)Math.Round(Math.Clamp(time - HitTime, -MissThreshold, MissThreshold));
             var isVisible = Alpha > 0;
-
-            //for (var noteIndex = Story.Notes.Children.Count - 1; noteIndex >= 0; --noteIndex) {
-            //    var note = Story.Notes.Children[noteIndex];
-
-            //    if (time < note.HitTime - Story.Notes.ShowTime) {
-            //        break;
-            //    }
-
-            //    if (note == this) { // Fix this Tomorrow Lol
-            //        Story.Notes.HasClickedNote = true;
-            //        break;
-            //    }
-            //}
             Story.Notes.HasClickedNote = true;
-
-            //var earliestNote = Story.Notes.Children.Last();
-            //var isEarliestNote = earliestNote == this;
-            Console.Write($"test: {HitTime}\n");
-            Console.Write($"EarliestNote: {Story.Notes.Children.Last().HitTime}\n");
-            return isVisible && Story.Notes.HasClickedNote;
+            return isVisible;
         }
 
         private void ClickNote() {
