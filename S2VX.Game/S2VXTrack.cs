c3105c3efe68c9d54153cfd84f70e96afda5fdb4
maxrchung <maxrchung@gmail.com>
2021-02-25 04:02:41 -0800

    Helper class to handle tracks and keep original path

---
+++ S2VX.Game/S2VXTrack.cs
@@ -0,0 +1,23 @@
+﻿using osu.Framework.Audio;
+using osu.Framework.Audio.Track;
+using osu.Framework.Graphics.Audio;
+using System.IO;
+
+namespace S2VX.Game {
+    /// <summary>
+    /// Wrapper around DrawableTrack so that we can keep reference to the audio path
+    /// </summary>
+    public class S2VXTrack : DrawableTrack {
+        public string AudioPath { get; }
+
+        public S2VXTrack(string audioPath, Track track) : base(track) =>
+            AudioPath = audioPath;
+
+        public static S2VXTrack Open(string audioPath, AudioManager audioManager) {
+            var trackStream = File.OpenRead(audioPath);
+            var track = new TrackBass(trackStream);
+            audioManager.AddItem(track);
+            return new S2VXTrack(audioPath, track);
+        }
+    }
+}
