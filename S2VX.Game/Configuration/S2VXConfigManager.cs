dd501647d922e03a10cd801ed44035abbd185020
Xenocidel <aliao4@uci.edu>
2021-03-23 21:34:36 -0700

    Add some comments

--- S2VX.Game/Configuration/S2VXConfigManager.cs
+++ S2VX.Game/Configuration/S2VXConfigManager.cs
@@ -3,8 +3,11 @@ using osu.Framework.Platform;
 using osu.Framework.Testing;
 
 namespace S2VX.Game.Configuration {
+    // This is the global settings manager, settings are saved on client machines
+    // This class is initialized and cached in S2VXGameBase and S2VXTestSceneTestRunner for headless tests
     [ExcludeFromDynamicCompile]
     public class S2VXConfigManager : IniConfigManager<S2VXSetting> {
+
         protected override void InitialiseDefaults() {
             // Gameplay
             SetDefault(S2VXSetting.HitErrorBarVisibility, false);
@@ -16,6 +19,7 @@ namespace S2VX.Game.Configuration {
 
     }
 
+    // Lists all settings
     public enum S2VXSetting {
         HitErrorBarVisibility,
         ScoreVisibility
