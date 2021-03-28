81a70e878e861b238d2fd99adfd21f3778731cf7
maxrchung <maxrchung@gmail.com>
2021-03-28 16:51:30 -0700

    Fix score processor tests

--- S2VX.Game.Tests/HeadlessTests/ScoreProcessorTests.cs
+++ S2VX.Game.Tests/HeadlessTests/ScoreProcessorTests.cs
@@ -20,49 +20,55 @@ namespace S2VX.Game.Tests.VisualTests {
 
         [Test]
         public void Process_PerfectHit_ColorsCursorPerfect() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Colors cursor perfect", () => Cursor.ActiveCursor.Colour = ScoreProcessor.PerfectColor);
         }
 
         [Test]
         public void Process_EarlyHit_ColorsCursorEarly() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000 - ScoreProcessor.PerfectThreshold - 1
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000 - ScoreProcessor.PerfectThreshold - 1,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Colors cursor early", () => Cursor.ActiveCursor.Colour = ScoreProcessor.EarlyColor);
         }
 
         [Test]
         public void Process_LateHit_ColorsCursorLate() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000 + ScoreProcessor.PerfectThreshold + 1
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000 + ScoreProcessor.PerfectThreshold + 1,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Colors cursor late", () => Cursor.ActiveCursor.Colour = ScoreProcessor.LateColor);
         }
 
         [Test]
         public void Process_MissHit_ColorsCursorMiss() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000 + ScoreProcessor.MissThreshold + 1
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000 + ScoreProcessor.MissThreshold + 1,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Colors cursor miss", () => Cursor.ActiveCursor.Colour = ScoreProcessor.MissColor);
         }
 
         [Test]
         public void Process_PerfectHit_PlaysHitSound() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Plays hit sound", () => ScoreProcessor.Hit.PlayCount = 1);
         }
 
         [Test]
         public void Process_MissHit_PlaysMissSound() {
-            AddStep("Process note", () => ScoreProcessor.Process(1000, new GameNote {
-                HitTime = 1000 - ScoreProcessor.MissThreshold - 1
-            }));
+            AddStep("Process note", () => ScoreProcessor.Process(
+                1000 - ScoreProcessor.MissThreshold - 1,
+                new GameNote { HitTime = 1000 }
+            ));
             AddAssert("Plays miss sound", () => ScoreProcessor.Miss.PlayCount = 1);
         }
     }
