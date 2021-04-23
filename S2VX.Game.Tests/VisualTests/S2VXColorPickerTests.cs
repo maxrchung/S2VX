fd55c498786357c396e89ab9147fb784a52031e4
Xenocidel <aliao4@uci.edu>
2021-04-22 20:36:28 -0700

    Added example bindable output for ColorPicker in visual test

--- S2VX.Game.Tests/VisualTests/S2VXColorPickerTests.cs
+++ S2VX.Game.Tests/VisualTests/S2VXColorPickerTests.cs
@@ -1,12 +1,16 @@
-
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
 using osu.Framework.Testing;
-using osuTK;
 using S2VX.Game.Editor.ColorPicker;
 
 namespace S2VX.Game.Tests.VisualTests {
     public class S2VXColorPickerTests : TestScene {
-        public S2VXColorPickerTests() =>
-            Add(new S2VXColorPicker { Margin = new MarginPadding(20) });
+        public S2VXColorPickerTests() {
+            S2VXColorPicker colorPicker;
+            SpriteText colorText;
+            Add(colorPicker = new S2VXColorPicker { Margin = new MarginPadding(20) });
+            Add(colorText = new SpriteText { });
+            colorPicker.Current.BindValueChanged(value => colorText.Text = value.NewValue.ToString());
+        }
     }
 }
