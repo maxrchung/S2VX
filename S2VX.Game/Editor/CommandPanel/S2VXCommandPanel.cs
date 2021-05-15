6fd4cf02f7018df19e6a40a17e9f7b485f39fff8
Xenocidel <aliao4@uci.edu>
2021-05-14 18:53:42 -0700

    Fix Editor dependency for ApplyCurrentTimeTests

--- S2VX.Game/Editor/CommandPanel/S2VXCommandPanel.cs
+++ S2VX.Game/Editor/CommandPanel/S2VXCommandPanel.cs
@@ -22,7 +22,7 @@ namespace S2VX.Game.Editor.CommandPanel {
         public static Vector2 InputSize { get; } = new Vector2(106, 30);
         public static float InputBarHeight { get; } = 70;
         private static Vector2 PanelSize { get; } = new Vector2(727, 727);
-        private CommandPanelInputBar AddInputBar { get; set; }
+        public CommandPanelInputBar AddInputBar { get; private set; }
         private CommandPanelInputBar EditInputBar { get; set; }
         private S2VXCommand EditCommandReference { get; set; }
         private readonly FillFlowContainer CommandsList = new() {
