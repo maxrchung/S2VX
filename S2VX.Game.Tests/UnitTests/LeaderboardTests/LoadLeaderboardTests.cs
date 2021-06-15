e4d5b08f238981a8c4646c25d58547463b5cf297
Xenocidel <aliao4@uci.edu>
2021-06-14 23:18:50 -0700

    Add leaderboard and start on unit-tests

---
+++ S2VX.Game.Tests/UnitTests/LeaderboardTests/LoadLeaderboardTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using S2VX.Game.Leaderboard;
+using System.IO;
+
+namespace S2VX.Game.Tests.UnitTests.LeaderboardTests {
+    [TestFixture]
+    public class LoadLeaderboardTests {
+
+        private readonly string StoryPath = Path.Combine("UnitTests", "LeaderboardTests", "story.s2ry");
+
+        [Test]
+        public void NoStoryboardFile_Has0Entries() {
+            var input = "doesNotExist.json";
+            var leaderboard = new LeaderboardContainer(StoryPath, input);
+            Assert.AreEqual(0, leaderboard.EntryCount);
+        }
+
+        //[Test]
+        //public void InvalidStoryboardFile_HasErrorMessage() {
+        //    var input = "invalid.json";
+        //    var leaderboard = new LeaderboardContainer(StoryPath, input);
+        //    Assert.AreEqual(0, leaderboard.EntryCount);
+        //}
+
+        //[Test]
+        //public void ValidStoryboardFile_Has3Entries() {
+        //    var input = "valid.json";
+        //    var leaderboard = new LeaderboardContainer(StoryPath, input);
+        //    Assert.AreEqual(3, leaderboard.EntryCount);
+        //}
+    }
+}
---
+++ S2VX.Game/Leaderboard/LeaderboardContainer.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osuTK.Graphics;
+using System.IO;
+
+namespace S2VX.Game.Leaderboard {
+    public class LeaderboardContainer : BasicScrollContainer {
+
+        private const string DefaultLeaderboardFileName = "leaderboard.json";
+        private TextFlowContainer NameColumn { get; set; }
+        private TextFlowContainer ScoreColumn { get; set; }
+        public int EntryCount { get; set; }
+
+        public LeaderboardContainer(string storyPath, string leaderboardFileName = DefaultLeaderboardFileName) {
+            var leaderboardPath = Path.Combine(Path.GetDirectoryName(storyPath), leaderboardFileName);
+            if (File.Exists(leaderboardPath)) {
+                LoadLeaderboard(leaderboardPath);
+            }
+        }
+
+        [BackgroundDependencyLoader]
+        private void Load() {
+            var textSize = SizeConsts.TextSize1;
+            Child = new Container {
+                Width = Width,
+                AutoSizeAxes = Axes.Y,
+                Children = new[] {
+                    NameColumn = new TextFlowContainer(s => s.Font = new FontUsage("default", textSize)) {
+                        AutoSizeAxes = Axes.Both,
+                        Anchor = Anchor.TopLeft,
+                        Origin = Anchor.TopLeft,
+                        TextAnchor = Anchor.TopLeft,
+                        Colour = Color4.White,
+                        Margin = new MarginPadding {
+                            Horizontal = textSize / 2,
+                        },
+                    },
+                    ScoreColumn = new TextFlowContainer(s => s.Font = new FontUsage("default", textSize)) {
+                        AutoSizeAxes = Axes.Both,
+                        Anchor = Anchor.TopRight,
+                        Origin = Anchor.TopRight,
+                        TextAnchor = Anchor.TopRight,
+                        Colour = Color4.White,
+                        Margin = new MarginPadding {
+                            Horizontal = textSize / 2,
+                        },
+                    },
+                },
+            };
+        }
+
+        private void LoadLeaderboard(string leaderboardPath) {
+            var text = File.ReadAllText(leaderboardPath);
+            var data = JsonConvert.DeserializeObject<LeaderboardEntries>(text);
+            foreach (var entry in data.Entries) {
+                NameColumn.AddParagraph(entry.Name);
+                ScoreColumn.AddParagraph(entry.Score);
+                ++EntryCount;
+            }
+        }
+    }
+}
