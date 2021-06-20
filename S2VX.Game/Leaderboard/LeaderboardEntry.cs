05108b6587d661e0e01b68712aac0841d7c0c271
Xenocidel <aliao4@uci.edu>
2021-06-19 19:32:42 -0700

    Add LeaderboardEntry.cs

---
+++ S2VX.Game/Leaderboard/LeaderboardEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace S2VX.Game.Leaderboard {
+    public class LeaderboardEntry {
+        public string Name { get; set; }
+        public string Score { get; set; }
+
+        public LeaderboardEntry(string name, string score) {
+            Name = name;
+            Score = score;
+        }
+    }
+}
