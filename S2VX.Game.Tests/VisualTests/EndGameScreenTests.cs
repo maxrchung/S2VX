c69113878608e0fcff9aadefa80211dc7c20713f
maxrchung <maxrchung@gmail.com>
2021-06-20 09:17:45 -0700

    Add initial end screen classes

---
+++ S2VX.Game.Tests/VisualTests/EndGameScreenTests.cs
@@ -0,0 +1,13 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Screens;
+using S2VX.Game.EndGame;
+using S2VX.Game.Play.Score;
+
+namespace S2VX.Game.Tests.VisualTests {
+    public class EndGameScreenTests : S2VXTestScene {
+        private ScoreStatistics ScoreStatistics { get; } = new();
+
+        [BackgroundDependencyLoader]
+        private void Load() => Add(new ScreenStack(new EndGameScreen(ScoreStatistics, "")));
+    }
+}
---
+++ S2VX.Game/EndGame/EndGameScreen.cs
@@ -0,0 +1,31 @@
+﻿using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.Screens;
+using osuTK.Graphics;
+using S2VX.Game.Play.Score;
+using System.Diagnostics.CodeAnalysis;
+
+// Using "EndGame" because "End" is a reserved word in C#
+namespace S2VX.Game.EndGame {
+    [SuppressMessage("CodeQuality", "IDE0052:Remove unread private members")]
+    public class EndGameScreen : Screen {
+        private ScoreStatistics ScoreStatistics { get; }
+        private string LeaderboardPath { get; }
+
+        public EndGameScreen(ScoreStatistics scoreStatistics, string leaderboardPath) {
+            ScoreStatistics = scoreStatistics;
+            LeaderboardPath = leaderboardPath;
+        }
+
+        [BackgroundDependencyLoader]
+        private void Load() =>
+            InternalChildren = new Drawable[] {
+                new Box {
+                    Colour = Color4.Red,
+                    Width = 300,
+                    Height = 300
+                }
+            };
+    }
+}
