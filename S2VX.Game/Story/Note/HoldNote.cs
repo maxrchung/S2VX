0a2ca690b16597e20f0df1311b2c730f82fac314
maxrchung <maxrchung@gmail.com>
2021-07-20 02:10:51 -0700

    Switch to use path radius sliders

--- S2VX.Game/Story/Note/HoldNote.cs
+++ S2VX.Game/Story/Note/HoldNote.cs
@@ -20,42 +20,21 @@ namespace S2VX.Game.Story.Note {
             }
         }
 
+        // MidCoordinates + EndCoordinates
+        // Used for calculations during update
+        private IEnumerable<Vector2> AllCoordinates { get; set; }
+        private void InitAllCoordinates() => AllCoordinates = MidCoordinates.Append(EndCoordinates);
+
         // Total distance of the hold note
         // Calculating this once on initialization will optimize position calculations during update
         private float TotalDistance { get; set; }
-        private void CalculateTotalDistance() {
-            var allCoordinates = MidCoordinates.Append(EndCoordinates);
+        private void InitTotalDistance() {
             var startCoordinate = Coordinates;
-            foreach (var endCoordinate in allCoordinates) {
-                var distance = (endCoordinate - startCoordinate).Length;
+            foreach (var coordinate in AllCoordinates) {
+                var distance = (coordinate - startCoordinate).LengthFast;
                 TotalDistance += distance;
-                startCoordinate = endCoordinate;
+                startCoordinate = coordinate;
             }
-        }
-
-        private Vector2 CalculateCurrentPosition(double time) {
-            // Find distance based on given time
-            var offsetFraction = S2VXUtils.ClampedInterpolation(time, 0, 1, HitTime, EndTime);
-            var offsetDistance = (float)offsetFraction * TotalDistance;
-
-            // Calculate offset position
-            var allCoordinates = MidCoordinates.Append(EndCoordinates);
-            var startCoordinate = Coordinates;
-            var totalDistance = 0f;
-            foreach (var endCoordinate in allCoordinates) {
-                var distance = (endCoordinate - startCoordinate).Length;
-
-                // If our position is within the current coordinate
-                if (totalDistance + distance > offsetDistance) {
-                    var remainingDistance = offsetDistance - totalDistance;
-                    var offsetCoordinates = S2VXUtils.ClampedInterpolation(remainingDistance, startCoordinate, endCoordinate, 0, distance);
-                    return offsetCoordinates;
-                } else {
-                    totalDistance += distance;
-                    startCoordinate = endCoordinate;
-                }
-            }
-            return EndCoordinates;
         }
 
         [Resolved]
@@ -63,7 +42,8 @@ namespace S2VX.Game.Story.Note {
 
         [BackgroundDependencyLoader]
         private void Load() {
-            CalculateTotalDistance();
+            InitAllCoordinates();
+            InitTotalDistance();
             AddInternal(SliderPath);
         }
 
@@ -87,79 +67,107 @@ namespace S2VX.Game.Story.Note {
             BoxOuter.Size = Vector2.One - cameraFactor * new Vector2(Story.Grid.Thickness);
             BoxInner.Size = BoxOuter.Size - 2 * cameraFactor * new Vector2(OutlineThickness);
 
-            var currCoordinates = CalculateCurrentPosition(Time.Current);
+            var currCoordinates = IndicatorCoordinates();
             Position = S2VXUtils.Rotate(currCoordinates - camera.Position, Rotation) * Size.X;
         }
 
-        protected Vector2 GetVertexEndPosition(float noteWidth, double time) {
-            var currCoordinates = S2VXUtils.ClampedInterpolation(time, Coordinates, EndCoordinates, HitTime, EndTime);
-            return (EndCoordinates - currCoordinates) * noteWidth;
+        private Vector2 IndicatorCoordinates() {
+            var startFraction = S2VXUtils.ClampedInterpolation(Time.Current, 0, 1, HitTime, EndTime);
+            var startDistance = (float)startFraction * TotalDistance;
+
+            var startCoordinate = Coordinates;
+            var totalDistance = 0f;
+            foreach (var coordinates in AllCoordinates) {
+                var distance = (coordinates - startCoordinate).Length;
+
+                if (totalDistance + distance > startDistance) {
+                    var remainingDistance = startDistance - totalDistance;
+                    var startCoordinates = S2VXUtils.ClampedInterpolation(remainingDistance, startCoordinate, coordinates, 0, distance);
+                    return startCoordinates;
+                } else {
+                    totalDistance += distance;
+                    startCoordinate = coordinates;
+                }
+            }
+            return EndCoordinates;
         }
 
         private void UpdateSliderPath() {
-            var drawWidth = S2VXGameBase.GameWidth;
-            var pathRadius = OutlineThickness * drawWidth / 2;
-            SliderPath.PathRadius = pathRadius;
+            UpdateVertices();
+
+            var noteWidth = Story.Camera.Scale.X * S2VXGameBase.GameWidth;
+            SliderPath.PathRadius = noteWidth / 2;
             SliderPath.Colour = Story.Notes.HoldNoteOutlineColor;
-
-            var time = Time.Current;
-            var notes = Story.Notes;
-            var camera = Story.Camera;
-            var noteWidth = camera.Scale.X * drawWidth;
-            Vector2 endPosition;
-            // If between Fade in time and Hit time, then snake out the slider end
-            if (time < HitTime) {
-                var startTime = HitTime - (EndTime - HitTime);
-                var snakeCoordinates = S2VXUtils.ClampedInterpolation(time, Vector2.Zero, EndCoordinates - Coordinates, startTime, HitTime);
-                endPosition = snakeCoordinates * noteWidth;
-            }
-            // Otherwise, snake in the slider start
-            else {
-                endPosition = GetVertexEndPosition(noteWidth, time);
-            }
-
-            var midPosition = endPosition / 2;
-            // Account for pathRadius in note width calculation
-            var fittedWidth = camera.Scale.X * drawWidth - pathRadius * 2;
-            var noteHalf = fittedWidth / 2;
-
-            var vertices = new List<Vector2>() {
-                new(-noteHalf, -noteHalf),
-                new(noteHalf, -noteHalf),
-                new(noteHalf, noteHalf),
-                new(-noteHalf, noteHalf),
-                endPosition + new Vector2(-noteHalf, -noteHalf),
-                endPosition + new Vector2(noteHalf, -noteHalf),
-                endPosition + new Vector2(noteHalf, noteHalf),
-                endPosition + new Vector2(-noteHalf, noteHalf)
-            };
-
-            // Sort based on descending distance to midPosition
-            vertices.Sort((left, right) =>
-                (right - midPosition).LengthSquared.CompareTo(
-                    (left - midPosition).LengthSquared
-                )
-            );
-
-            // Drop the last 2 (the closest) vertices
-            vertices.RemoveAt(vertices.Count - 1);
-            vertices.RemoveAt(vertices.Count - 1);
-
-            // Sort based on relative angle to unit vector
-            vertices.Sort((left, right) =>
-                S2VXUtils.AngleBetween(Vector2.UnitX, left - midPosition).CompareTo(
-                    S2VXUtils.AngleBetween(Vector2.UnitX, right - midPosition)
-                )
-            );
-
-            // Connect last vertex back to first
-            vertices.Add(vertices.First());
-
-            SliderPath.Vertices = vertices;
-            // ?????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-            // wtf this black magic shit is wack
             // https://github.com/ppy/osu/blob/86cf42d6107246842a30d9802a4a4fc1c09720c7/osu.Game.Rulesets.Osu/Skinning/Default/SnakingSliderBody.cs#L149
             SliderPath.Position = -SliderPath.PositionInBoundingBox(Vector2.Zero);
+        }
+
+        private void UpdateVertices() {
+            var vertices = Time.Current < HitTime
+                ? SnakeInVertices()
+                : SnakeOutVertices();
+            SliderPath.Vertices = vertices;
+        }
+
+        private List<Vector2> SnakeInVertices() {
+            var vertices = new List<Vector2>();
+            var startTime = HitTime - (EndTime - HitTime);
+            var endFraction = S2VXUtils.ClampedInterpolation(Time.Current, 0, 1, startTime, HitTime);
+            var endDistance = (float)endFraction * TotalDistance;
+            var noteWidth = Story.Camera.Scale.X * S2VXGameBase.GameWidth;
+
+            vertices.Add(Vector2.Zero);
+
+            var startCoordinates = Coordinates;
+            var totalDistance = 0f;
+            foreach (var coordinates in AllCoordinates) {
+                var distance = (coordinates - startCoordinates).LengthFast;
+
+                if (totalDistance + distance > endDistance) {
+                    var remainingDistance = endDistance - totalDistance;
+                    var offsetCoordinates = S2VXUtils.ClampedInterpolation(remainingDistance, startCoordinates, coordinates, 0, distance);
+                    // Subtract Coordinates so that path is positioned relative to start coordinate
+                    vertices.Add((offsetCoordinates - Coordinates) * noteWidth);
+                    break;
+                } else {
+                    vertices.Add((coordinates - Coordinates) * noteWidth);
+                    totalDistance += distance;
+                    startCoordinates = coordinates;
+                }
+            }
+            return vertices;
+        }
+
+        private List<Vector2> SnakeOutVertices() {
+            var vertices = new List<Vector2>();
+            var startFraction = S2VXUtils.ClampedInterpolation(Time.Current, 0, 1, HitTime, EndTime);
+            var startDistance = (float)startFraction * TotalDistance;
+            var noteWidth = Story.Camera.Scale.X * S2VXGameBase.GameWidth;
+
+            var startCoordinates = Coordinates;
+            var totalDistance = 0f;
+            var hasFoundStart = false;
+            var offsetCoordinates = new Vector2();
+            foreach (var coordinates in AllCoordinates) {
+                var distance = (coordinates - startCoordinates).LengthFast;
+
+                if (!hasFoundStart) {
+                    if (totalDistance + distance > startDistance) {
+                        var remainingDistance = startDistance - totalDistance;
+                        offsetCoordinates = S2VXUtils.ClampedInterpolation(remainingDistance, startCoordinates, coordinates, 0, distance);
+                        vertices.Add(Vector2.Zero);
+                        hasFoundStart = true;
+                    }
+                } else {
+                    vertices.Add((coordinates - offsetCoordinates) * noteWidth);
+                }
+
+                totalDistance += distance;
+                startCoordinates = coordinates;
+            }
+
+            vertices.Add((EndCoordinates - offsetCoordinates) * noteWidth);
+            return vertices;
         }
     }
 }
