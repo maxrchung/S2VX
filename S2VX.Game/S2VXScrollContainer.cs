c66951e9e2af150b5682735ad72a685434038999
Naranja-Sagged <alexliuying@gmail.com>
2021-07-29 20:58:02 -0700

    Add custom scroll container

---
+++ S2VX.Game/S2VXScrollContainer.cs
@@ -0,0 +1,10 @@
+﻿using osu.Framework.Graphics.Containers;
+using osu.Framework.Input.Events;
+
+namespace S2VX.Game {
+    public class S2VXScrollContainer : BasicScrollContainer {
+        public S2VXScrollContainer() { }
+
+        protected override bool OnScroll(ScrollEvent e) => false;
+    }
+}
