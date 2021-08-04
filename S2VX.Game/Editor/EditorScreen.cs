e5453882227fef1493a7962173eb154a39309660
Naranja-Sagged <alexliuying@gmail.com>
2021-08-04 16:47:42 -0700

    Remove VolumeDisplay.cs

--- S2VX.Game/Editor/EditorScreen.cs
+++ S2VX.Game/Editor/EditorScreen.cs
@@ -449,10 +449,7 @@ namespace S2VX.Game.Editor {
 
         // Note that Volume is set in a special framework.ini that is unique to your computer,
         // for example my path is: C:\Users\Wax Chug da Gwad\AppData\Roaming\S2VX\framework.ini
-        private void VolumeSet(double volume = 1.0) {
-            Audio.Volume.Value = volume;
-            EditorInfoBar.VolumeDisplay.UpdateDisplay();
-        }
+        private void VolumeSet(double volume = 1.0) => Audio.Volume.Value = volume;
 
         public void VolumeIncrease(double step = 0.1) => VolumeSet(Audio.Volume.Value + step);
 
