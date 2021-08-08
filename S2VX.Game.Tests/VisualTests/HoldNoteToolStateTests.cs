75a5895db298c1fc4e2087e8db19059c84e1f175
maxrchung <maxrchung@gmail.com>
2021-08-08 13:34:15 -0700

    Outline tests for hold note editing

---
+++ S2VX.Game.Tests/VisualTests/HoldNoteToolStateTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Screens;
+using osu.Framework.Testing;
+using osuTK.Input;
+using S2VX.Game.EndGame;
+using S2VX.Game.Play;
+using S2VX.Game.SongSelection;
+using System.IO;
+
+namespace S2VX.Game.Tests.VisualTests {
+    public class HoldNoteToolStateTests : S2VXTestScene {
+
+        // Control case - no hold notes
+        // Left click - escape - no hold notes
+        // Left click right click at same time - no hold note created
+        // Left click right click - hold note created
+        // Left click left click right click - hold note created with anchors
+        // Left click left click right click ctrl z - hold note removed
+        // Left click left click right click ctrl z+ctrl+shift+z - hold note readded
+
+    }
+}
---
+++ S2VX.Game.Tests/VisualTests/SelectToolStateTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Screens;
+using osu.Framework.Testing;
+using osuTK.Input;
+using S2VX.Game.EndGame;
+using S2VX.Game.Play;
+using S2VX.Game.SongSelection;
+using System.IO;
+
+namespace S2VX.Game.Tests.VisualTests {
+    public class SelectToolStateTests : S2VXTestScene {
+        // With hold note, adjust start point - start point modified
+        // Ctrl+z - start point removed
+        // Ctrl+shift+z - start point readded
+        // With hold note, adjust end point - end point modified
+        // Ctrl+z - end point removed
+        // Ctrl+shift+z - end point readded
+        // With hold note with multiple mid coordinates, adjust mid coordinate - mid coordinate modified
+        // Ctrl+z - mid point removed
+        // Ctrl+shift+z - mid point readded
+    }
+}
