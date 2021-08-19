b24c320a6eeca73f1c83adadb1376ec77509d168
maxrchung <maxrchung@gmail.com>
2021-08-18 20:30:40 -0700

    Flesh out unit tests

--- S2VX.Game.Tests/HeadlessTests/TapPanelTests.cs
+++ S2VX.Game.Tests/HeadlessTests/TapPanelTests.cs
@@ -4,8 +4,10 @@ using osu.Framework.Audio;
 using osu.Framework.Screens;
 using osu.Framework.Testing;
 using osu.Framework.Timing;
+using osuTK.Input;
 using S2VX.Game.Editor;
 using S2VX.Game.Editor.Containers;
+using S2VX.Game.Editor.UserInterface;
 using S2VX.Game.Story;
 using S2VX.Game.Story.Command;
 using S2VX.Game.Story.Note;
@@ -15,6 +17,8 @@ namespace S2VX.Game.Tests.HeadlessTests {
     [HeadlessTest]
     public class TapPanelTests : S2VXTestScene {
         private TapPanel TapPanel { get; set; }
+        private TapReceptor TapReceptor { get; set; }
+
 
         private StopwatchClock TapClock { get; set; }
 
@@ -25,6 +29,7 @@ namespace S2VX.Game.Tests.HeadlessTests {
             var editor = new EditorScreen(new S2VXStory(), S2VXTrack.Open(audioPath, audio));
             TapPanel = editor.TapPanel;
             TapPanel.Clock = new FramedClock(TapClock);
+            TapReceptor = TapPanel.TapReceptor;
             Add(new ScreenStack(editor));
         }
 
@@ -32,39 +37,68 @@ namespace S2VX.Game.Tests.HeadlessTests {
         [SetUpSteps]
         public void SetUpSteps() {
             AddStep("Reset clock", () => TapPanel.Clock = new FramedClock(TapClock));
-            AddStep("Reset tap panel", () => TapPanel.TapReceptor.Reset());
+            AddStep("Reset tap panel", () => TapReceptor.Reset());
+            AddStep("Move mouse", () => InputManager.MoveMouseTo(TapReceptor));
+        }
+
+        public void MouseClick() => AddStep("Click", () => InputManager.Click());
+        public void KeyUp() => AddStep("Key up", () => InputManager.PressKey(Key.F));
+        public void Seek(double time) => AddStep($"Seek to {time}", () => TapClock.Seek(time));
+        public void AssertTaps(int taps) => AddAssert($"Is {taps} taps", () => TapReceptor.TapsLabel.Value == $"Taps: {taps}");
+        public void AssertBPM(int bpm) => AddAssert($"Is {bpm} BPM", () => TapReceptor.BPMLabel.Value == $"BPM: {bpm}");
+
+        [Test]
+        public void ProcessTap_0Taps_Is0Taps() => AssertTaps(0);
+
+        [Test]
+        public void ProcessTap_0Taps_Is0BPM() => AssertTaps(0);
+
+        [Test]
+        public void ProcessTap_1ClickIn1Second_Is1Tap() {
+            MouseClick();
+            AssertTaps(1);
         }
 
         [Test]
-        public void ProcessTap_0Taps_Is0Taps() =>
-            AddAssert("Is 0 taps", () => TapPanel.TapReceptor.TapsLabel.Value == "Taps: 0");
+        public void ProcessTap_1ClickIn1Second_Is0BPM() {
+            MouseClick();
+            AssertBPM(0);
+        }
 
         [Test]
-        public void ProcessTap_0Taps_Is0BPM() =>
-            AddAssert("Is 0 taps", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
+        public void ProcessTap_2KeyUpsIn1Second_Is2Taps() {
+            KeyUp();
+            Seek(1000);
+            KeyUp();
+            AssertTaps(2);
+        }
 
         [Test]
-        public void ProcessTap_1ClickIn1Second_Is1Tap() =>
-            AddAssert("Is 0 taps", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
+        public void ProcessTap_2KeyUpsIn1Second_Is60BPM() {
+            KeyUp();
+            Seek(1000);
+            KeyUp();
+            AssertBPM(60);
+        }
 
         [Test]
-        public void ProcessTap_1KeyUpIn1Second_Is1Tap() =>
-            AddAssert("Is 0 taps", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
+        public void ProcessTap_3TapsIn1Second_Is3Taps() {
+            MouseClick();
+            Seek(500);
+            KeyUp();
+            Seek(1000);
+            MouseClick();
+            AssertTaps(3);
+        }
 
         [Test]
-        public void ProcessTap_1ClickIn1Second_Is60BPM() =>
-            AddAssert("Is 0 taps", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
-
-        [Test]
-        public void ProcessTap_1KeyUpIn1Second_Is60BPM() =>
-            AddAssert("Is 60 BPM", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
-
-        [Test]
-        public void ProcessTap_1Click1KeyUpIn1Second_Is2Taps() =>
-            AddAssert("Is 2 taps", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
-
-        [Test]
-        public void ProcessTap_1Click1KeyUpIn1Second_Is120BPM() =>
-            AddAssert("Is 120 BPM", () => TapPanel.TapReceptor.BPMLabel.Value == "BPM: 0");
+        public void ProcessTap_3TapsIn1Second_Is120BPM() {
+            KeyUp();
+            Seek(500);
+            MouseClick();
+            Seek(1000);
+            KeyUp();
+            AssertBPM(120);
+        }
     }
 }
