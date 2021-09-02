99eb8af1885bf456811c3ede46b42b22eb65ade6
Xenocidel <aliao4@uci.edu>
2021-09-02 00:15:01 -0700

    Remove commented out code

--- S2VX.Game/Editor/UserInterface/S2VXDropdown.cs
+++ S2VX.Game/Editor/UserInterface/S2VXDropdown.cs
@@ -3,8 +3,6 @@
 namespace S2VX.Game.Editor.UserInterface {
     public class S2VXDropdown<T> : BasicDropdown<T> {
 
-        //protected override DropdownHeader CreateHeader() => new OsuDropdownHeader();
-
         protected override DropdownMenu CreateMenu() => new S2VXDropdownMenu();
 
         protected class S2VXDropdownMenu : BasicDropdownMenu {
