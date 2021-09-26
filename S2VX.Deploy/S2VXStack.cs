e6d8323c800feb71476cd52c209de3750aef15ed
maxrchung <maxrchung@gmail.com>
2021-09-25 20:24:15 -0700

    Add back amplify.yml since BuildSpec syntax is way too wacky

--- S2VX.Deploy/S2VXStack.cs
+++ S2VX.Deploy/S2VXStack.cs
@@ -1,8 +1,6 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.Amplify;
-using Amazon.CDK.AWS.CodeBuild;
 using Amazon.CDK.AWS.Route53;
-using System.Collections.Generic;
 using Amplify = Amazon.CDK.AWS.Amplify;
 
 namespace S2VX.Deploy {
@@ -19,35 +17,6 @@ namespace S2VX.Deploy {
                     OauthToken = SecretValue.PlainText(
                         System.Environment.GetEnvironmentVariable("PERSONAL_ACCESS_TOKEN")
                     )
-                }),
-                // https://swimburger.net/blog/dotnet/how-to-deploy-blazor-webassembly-to-aws-amplify
-                BuildSpec = BuildSpec.FromObjectToYaml(new Dictionary<string, object> {
-                    { "Version", 1 },
-                    {
-                        "Applications",
-                        new {
-                            Frontend = new {
-                                Phases = new {
-                                    PreBuild = new {
-                                        Commands = new[] {
-                                            "curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh",
-                                            "chmod +x *.sh",
-                                            "./dotnet-install.sh -c 5.0 -InstallDir ./dotnet5",
-                                            "./dotnet5/dotnet --version"
-                                        }
-                                    },
-                                    Build = new {
-                                        Commands = new[] { "./dotnet5/dotnet publish -c Release -o release" }
-                                    }
-                                },
-                                Artifacts = new {
-                                    BaseDirectory = "/release/wwwroot",
-                                    Files = "**/*"
-                                }
-                            },
-                            AppRoot = "S2VX.Web"
-                        }
-                    }
                 }),
             });
 
