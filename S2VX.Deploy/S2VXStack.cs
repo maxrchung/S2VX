613c61741ec488640bcd07dfb0c5e914921bf3cb
maxrchung <maxrchung@gmail.com>
2021-09-25 21:56:06 -0700

    Remove hosted zone

--- S2VX.Deploy/S2VXStack.cs
+++ S2VX.Deploy/S2VXStack.cs
@@ -6,20 +6,19 @@ using Amplify = Amazon.CDK.AWS.Amplify;
 namespace S2VX.Deploy {
     public class S2VXStack : Stack {
         internal S2VXStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props) {
-            new PublicHostedZone(this, "S2VX.HostedZone", new PublicHostedZoneProps {
-                ZoneName = "s2vx.com"
-            });
-
             var app = new Amplify.App(this, "S2VX.Amplify", new Amplify.AppProps {
+                // Configurations for Amplify to find and access our repo
                 SourceCodeProvider = new GitHubSourceCodeProvider(new GitHubSourceCodeProviderProps {
                     Owner = "maxrchung",
                     Repository = "S2VX",
                     OauthToken = SecretValue.PlainText(
+                        // Personal access token generated for my GitHub account
                         System.Environment.GetEnvironmentVariable("PERSONAL_ACCESS_TOKEN")
                     )
                 }),
             });
 
+            // Links our s2vx.com domain to the repository branch
             var branch = app.AddBranch("amplify-deploy");
             var domain = app.AddDomain("s2vx.com");
             domain.MapRoot(branch);
