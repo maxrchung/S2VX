06d96fb41b0359d115f2cb3910c3fc89b347dd20
maxrchung <maxrchung@gmail.com>
2021-10-19 23:08:17 -0700

    Add guard clause to prevent updating anchors

--- S2VX.Game/Story/Note/EditorHoldNoteAnchor.cs
+++ S2VX.Game/Story/Note/EditorHoldNoteAnchor.cs
@@ -1,6 +1,8 @@
 ﻿using osu.Framework.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osuTK;
+using S2VX.Game.Editor;
+using S2VX.Game.Editor.ToolState;
 
 namespace S2VX.Game.Story.Note {
     public class EditorHoldNoteAnchor : Box {
@@ -15,5 +17,30 @@ namespace S2VX.Game.Story.Note {
             Note = note;
             Size = new(AnchorWidth);
         }
+
+        /// <summary>
+        /// It's possible to crash the game while creating a hold note. Here's
+        /// an example scenario:
+        /// 1). User selects the hold note tool.
+        /// 2). User places a start anchor.
+        /// 3). User scrolls the timeline forward
+        /// 4). User clicks and drags when placing a second anchor.
+        ///
+        /// During a hold note's creation, the hold note's approach is not set.
+        /// In step 4, when a user clicks and drags, the drag actually triggers
+        /// the end anchor's OnDrag and tries to update the hold note's
+        /// coordinates and approach coordinates. Because the approach doesn't
+        /// exist, the game throws a null exception.
+        ///
+        /// Two ways we can solve this is either adding a guard clause so that
+        /// only the select tool can perform anchor updates or adding null
+        /// checks within the hold note class's update functions. I opted for
+        /// the former since clicking with the hold note tool should only create
+        /// or manipulate hold notes. It feels confusing to me if a click also
+        /// updates anchor. That seems more like the job exclusive for select
+        /// tool.
+        /// </summary>
+        protected static bool CanUpdateAnchor(EditorScreen editor) =>
+            editor.ToolState is SelectToolState;
     }
 }
